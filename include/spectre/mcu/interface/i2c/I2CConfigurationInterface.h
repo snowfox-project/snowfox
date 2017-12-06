d023c91b49c7c45ca9761afae8553fbdfeaa563b
Alexander Entinger <cto@lxrobotics.com>
2017-12-06 13:25:19 +0100

    Adding I2CInterface definition

---
+++ include/spectre/mcu/interface/i2c/I2CConfigurationInterface.h
@@ -0,0 +1,66 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace mcu
+{
+
+namespace interface
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class I2CConfigurationInterface
+{
+
+public:
+
+  typedef enum
+  {
+    F_100_kHz   /* Standard mode  */
+    F_400_kHz   /* Fast mode      */
+    F_1000_kHz  /* Fast mode plus */
+  } eBusClock;
+
+
+           I2CConfigurationInterface() { }
+  virtual ~I2CConfigurationInterface() { }
+
+
+  virtual void setSpeed(eBusClock const bus_clock) = 0;
+  
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* interface*/
+
+} /* mcu */
+
+} /* spectre */
+
---
+++ include/spectre/mcu/interface/i2c/I2CInterface.h
@@ -0,0 +1,68 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace mcu
+{
+
+namespace interface
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class I2CInterface
+{
+
+public:
+
+           I2CInterface() { }
+  virtual ~I2CInterface() { }
+
+  virtual bool begin      (uint8_t const address, bool const is_read_access               ) = 0;
+  virtual void end        (                                                               ) = 0;
+
+  virtual bool write      (uint8_t const data                                             ) = 0;
+  virtual bool requestFrom(uint8_t const address, uint8_t * data, uint16_t const num_bytes) = 0;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* interface*/
+
+} /* mcu */
+
+} /* spectre */
+
