f2be0b0276f8ba083a14632b5aafb13f6ac68130
Alexander Entinger <cto@lxrobotics.com>
2017-12-18 09:15:49 +0100

    Minor cosmetic changes

--- src/hal/avr/ATxxxx/DigitalOutPin.cpp
+++ src/hal/avr/ATxxxx/DigitalOutPin.cpp
@@ -39,10 +39,10 @@ namespace ATxxxx
  * CTOR/DTOR
  **************************************************************************************/
 
-DigitalOutPin::DigitalOutPin(volatile uint8_t *ddr, volatile uint8_t *out, uint8_t const out_pin_number) :
-    _ddr              (ddr                ),
-    _out              (out                ),
-    _out_pin_bitmask  (1 << out_pin_number)
+DigitalOutPin::DigitalOutPin(volatile uint8_t *ddr, volatile uint8_t *out, uint8_t const out_pin_number)
+: _ddr              (ddr                ),
+  _out              (out                ),
+  _out_pin_bitmask  (1 << out_pin_number)
 {
   setGpioPinAsOutput();
 }
