2a0519f436e2c98e001ad2112ec07770f61c0625
Alexander Entinger <cto@lxrobotics.com>
2017-12-20 08:19:54 +0100

    Move script run_cppcheck.sh into dedicated scripts folder

--- .ci/scripts/run_cppcheck.sh
+++ .ci/scripts/run_cppcheck.sh
@@ -3,13 +3,13 @@
 cd $(dirname $(readlink -f $0))
 
 if [ "$1" = "-out=xml" ]; then
-  mkdir -p ../build/report/cppcheck
-  cd ../build/report/cppcheck
+  mkdir -p ../../build/report/cppcheck
+  cd ../../build/report/cppcheck
   cppcheck --enable=warning --xml-version=2 ../../../src 2> cppcheck.xml
   cppcheck-htmlreport --file=cppcheck.xml --report-dir=./ --source-dir=../../../src
 fi
 
 if [ "$1" = "-out=gcc" ]; then
-  cppcheck --enable=warning --template=gcc ../src
+  cppcheck --enable=warning --template=gcc ../../src
 fi
 
