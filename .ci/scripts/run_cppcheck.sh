a624787448b16afdfcb85526d54eacc96e1f2fc3
Alexander Entinger <cto@lxrobotics.com>
2017-12-20 13:17:47 +0100

    Changing run_cppcheck script to also run linting on the examples

--- .ci/scripts/run_cppcheck.sh
+++ .ci/scripts/run_cppcheck.sh
@@ -5,11 +5,11 @@ cd $(dirname $(readlink -f $0))
 if [ "$1" = "-out=xml" ]; then
   mkdir -p ../../build/report/cppcheck
   cd ../../build/report/cppcheck
-  cppcheck --enable=warning --xml-version=2 ../../../src 2> cppcheck.xml
-  cppcheck-htmlreport --file=cppcheck.xml --report-dir=./ --source-dir=../../../src
+  cppcheck --enable=warning --xml-version=2 ../../../src ../../../examples 2> cppcheck.xml
+  cppcheck-htmlreport --file=cppcheck.xml --report-dir=./ --source-dir=../../../src ../../../examples
 fi
 
 if [ "$1" = "-out=gcc" ]; then
-  cppcheck --enable=warning --template=gcc ../../src
+  cppcheck --enable=warning --template=gcc ../../src ../../examples
 fi
 
