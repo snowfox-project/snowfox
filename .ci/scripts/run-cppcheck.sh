87c9bacddfa2a4fe638eaafc96e7bcafe9e3159d
Alexander Entinger <cto@lxrobotics.com>
2018-01-11 16:10:07 +0100

    Adding error message to script run-cppcheck.sh

--- .ci/scripts/run-cppcheck.sh
+++ .ci/scripts/run-cppcheck.sh
@@ -2,14 +2,20 @@
 
 cd $(dirname $(readlink -f $0))
 
-if [ "$1" = "-out=xml" ]; then
+if   [ "$1" = "-out=xml" ]; then
+
   mkdir -p ../../build/report/cppcheck
   cd ../../build/report/cppcheck
   cppcheck --enable=warning --xml-version=2 ../../../src ../../../examples 2> cppcheck.xml
   cppcheck-htmlreport --file=cppcheck.xml --report-dir=./ --source-dir=../../../src ../../../examples
-fi
 
-if [ "$1" = "-out=gcc" ]; then
+elif [ "$1" = "-out=gcc" ]; then
+
   cppcheck --enable=warning --template=gcc ../../src ../../examples
+
+else
+
+  echo "$(basename "$0") -out=[gcc|xml]" >&2
+
 fi
 
