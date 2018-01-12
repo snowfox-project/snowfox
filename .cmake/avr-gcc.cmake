aea052c2858d526abd330d13dc39db27ef8aced5
Alexander Entinger <cto@lxrobotics.com>
2018-01-12 08:18:29 +0100

    Moved definitions for avr-gcc from the main CMakeLists.txt to avr-gcc.cmake

--- .cmake/avr-gcc.cmake
+++ .cmake/avr-gcc.cmake
@@ -5,6 +5,20 @@ SET(CMAKE_CXX_COMPILER avr-g++)
 
 ##########################################################################
 
+add_definitions(-mmcu=${MCU_TYPE})
+add_definitions(-Os)
+add_definitions(-Wall)
+add_definitions(-pedantic)
+add_definitions(-pedantic-errors)
+add_definitions(-funsigned-char)
+add_definitions(-funsigned-bitfields)
+add_definitions(-fshort-enums)
+add_definitions(-ffunction-sections)
+add_definitions(-fdata-sections)
+add_definitions(-fno-exceptions)
+
+##########################################################################
+
 SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-Wl,--relax,--gc-sections")
 
 ##########################################################################
