47a6692ea71afb328881dd1ce1e74eeb193cfc1b
Alexander Entinger <cto@lxrobotics.com>
2018-01-15 17:29:07 +0100

    Renamed build scripts for better readability

--- .ci/run-install.sh
+++ .ci/run-install.sh
@@ -3,7 +3,7 @@ cd $(dirname $(readlink -f $0))
 
 sudo apt-get install cmake
 
-sudo apt-get install python cppcheck
+sudo apt-get install cppcheck
 
 sudo apt-get install avr-libc binutils-avr gcc-avr avrdude
 
