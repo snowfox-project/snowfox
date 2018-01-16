f7b2e14f620e485c18cea242a5989d6b12a0567f
Alexander Entinger <cto@lxrobotics.com>
2018-01-16 08:13:27 +0100

    Updated install script to install lcov and coverall conversion gem

--- .ci/run-install.sh
+++ .ci/run-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 cd $(dirname $(readlink -f $0))
 
-sudo apt-get install cmake
-
 sudo apt-get install cppcheck
+sudo apt-get install lcov
+gem install coveralls-lcov
+lcov --version
+g++ --version
 
+sudo apt-get install cmake
 sudo apt-get install avr-libc binutils-avr gcc-avr avrdude
+
+
 
