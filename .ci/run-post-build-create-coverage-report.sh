d8e7c0695dfc5429981883050ba197b923e3feb2
Alexander Entinger <cto@lxrobotics.com>
2018-01-16 12:52:09 +0100

    Fixed some more typos

--- .ci/run-post-build-create-coverage-report.sh
+++ .ci/run-post-build-create-coverage-report.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $(readlink -f $0))
 
-cd ../../build
+cd ../build
 
-- lcov --directory . --capture --output-file coverage.info
-- lcov --remove coverage.info 'test/*' '/usr/*' --output-file coverage.info
-- lcov --list coverage.info
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info 'test/*' '/usr/*' --output-file coverage.info
+lcov --list coverage.info
