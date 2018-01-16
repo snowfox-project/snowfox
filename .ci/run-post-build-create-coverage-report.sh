26967bc184bb18a1dd89bda6f62720e6d7b798f6
Alexander Entinger <cto@lxrobotics.com>
2018-01-16 12:42:09 +0100

    Adding scripts for creation of coverage reports

---
+++ .ci/run-post-build-create-coverage-report.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $(dirname $(readlink -f $0))
+
+cd ../../build
+
+- lcov --directory . --capture --output-file coverage.info
+- lcov --remove coverage.info 'test/*' '/usr/*' --output-file coverage.info
+- lcov --list coverage.info
---
+++ .ci/run-pre-build-prepare-create-coverage-report.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $(dirname $(readlink -f $0))
+
+cd ../../build
+
+- lcov --directory . --zerocounters
