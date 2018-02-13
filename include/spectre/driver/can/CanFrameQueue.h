a8781744c4c45de918d1f74da1e1db52a8592ddf
Alexander Entinger <cto@lxrobotics.com>
2018-02-13 12:49:45 +0100

    Adding typedef for a CAN frame queue

---
+++ include/spectre/driver/can/CanFrameQueue.h
@@ -0,0 +1,58 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_DRIVER_CAN_CANFRAMEQUEUE_H_
+#define INCLUDE_SPECTRE_DRIVER_CAN_CANFRAMEQUEUE_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/memory/container/Queue.h>
+#include <spectre/hal/interface/can/CanFrame.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace driver
+{
+
+namespace can
+{
+
+/**************************************************************************************
+ * TYPEDEFS
+ **************************************************************************************/
+
+typedef memory::container::Queue<hal::interface::CanFrame> CanFrameQueue;
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* can */
+
+} /* driver */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_DRIVER_CAN_CANFRAMEQUEUE_H_ */
