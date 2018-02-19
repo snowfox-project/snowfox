24e0771bcb735c82f1135dc74c3d301a016e6442
Alexander Entinger <cto@lxrobotics.com>
2018-02-19 07:29:42 +0100

    Adding first skeleton of C++ list - needed for the os part

---
+++ include/spectre/memory/container/List.h
@@ -0,0 +1,95 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_MEMORY_CONTAINER_LIST_H_
+#define INCLUDE_SPECTRE_MEMORY_CONTAINER_LIST_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace memory
+{
+
+namespace container
+{
+
+/**************************************************************************************
+ * TYPEDEFS
+ **************************************************************************************/
+
+template <typename T>
+struct ListNode
+{
+  T          data;
+  ListNode * prev,
+           * next;
+};
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+template <class T>
+class List
+{
+
+public:
+
+   List();
+  ~List();
+
+
+  inline ListNode<T> * front() { return _head; }
+  inline ListNode<T> * back () { return _tail; }
+
+
+private:
+
+  ListNode<T> * _head,
+              * _tail;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* container*/
+
+} /* memory */
+
+} /* spectre */
+
+/**************************************************************************************
+ * TEMPLATE CODE IMPLEMENTATION
+ **************************************************************************************/
+
+#include "List.ipp"
+
+#endif /* INCLUDE_SPECTRE_MEMORY_CONTAINER_LIST_H_ */
