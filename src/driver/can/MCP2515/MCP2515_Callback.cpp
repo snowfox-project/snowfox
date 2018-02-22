01efb86d9986ceff29be1b4d7c944a46c9e8f6da
Alexander Entinger <cto@lxrobotics.com>
2018-02-22 08:12:00 +0100

    Adding class to be executed upon an external interrupt event of the MCP2515

---
+++ src/driver/can/MCP2515/MCP2515_Callback.cpp
@@ -0,0 +1,74 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/driver/can/MCP2515/MCP2515_Callback.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace driver
+{
+
+namespace can
+{
+
+namespace MCP2515
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+MCP2515_Callback::MCP2515_Callback()
+{
+
+}
+
+MCP2515_Callback::~MCP2515_Callback()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void MCP2515_Callback::onExternalEventCallback()
+{
+  /* TODO - not yet sure how to marry everything together */
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* MCP2515 */
+
+} /* can */
+
+} /* driver */
+
+} /* spectre */
