25adaa26de4ed1bb22b155a231aef2eea73967fb
lxrobotics <cto@lxrobotics.com>
2018-03-13 09:58:24 +0100

    Adding HAL driver for ATMEGA2560::TIMER2

---
+++ include/spectre/hal/avr/ATMEGA2560/TIMER2.h
@@ -0,0 +1,103 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_ATMEGA2560_TIMER2_H_
+#define INCLUDE_SPECTRE_HAL_AVR_ATMEGA2560_TIMER2_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <stdint.h>
+
+#include <spectre/hal/interface/timer/Timer.h>
+#include <spectre/hal/interface/timer/TimerConfiguration.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA2560
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class TIMER2 : public interface::Timer<uint8_t>,
+               public interface::TimerConfiguration
+{
+
+public:
+
+           TIMER2(volatile uint8_t * TCNT2,
+                  volatile uint8_t * TCCR2A,
+                  volatile uint8_t * OCR2A,
+                  volatile uint8_t * OCR2B);
+  virtual ~TIMER2();
+
+
+  static uint8_t const COMPARE_A = 0;
+  static uint8_t const COMPARE_B = 1;
+
+
+  /* Timer Interface */
+
+  virtual void    start             (                 ) override;
+  virtual void    stop              (                 ) override;
+  virtual void    set               (uint8_t const val) override;
+  virtual uint8_t get               (                 ) override;
+
+  virtual void    setCompareRegister(uint8_t const reg_sel, uint8_t const reg_val) override;
+
+
+  /* Timer Configuration Interface */
+
+  virtual void setPrescaler(uint32_t const prescaler) override;
+
+private:
+
+           uint32_t   _prescaler;
+
+  volatile uint8_t  * _TCNT2,
+                    * _TCCR2A,
+                    * _OCR2A,
+                    * _OCR2B;
+
+
+  void setPrescaler_TCCR2A(uint32_t const prescaler);
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA2560 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_ATMEGA2560_TIMER2_H_ */
---
+++ src/hal/avr/ATMEGA2560/TIMER2.cpp
@@ -0,0 +1,152 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/hal/avr/ATMEGA2560/TIMER2.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA2560
+{
+
+/**************************************************************************************
+ * DEFINES
+ **************************************************************************************/
+
+/* TCCR2A */
+#define CS20_bm   (1<<0)
+#define CS21_bm   (1<<1)
+#define CS22_bm   (1<<2)
+
+/**************************************************************************************
+ * TYPEDEFS
+ **************************************************************************************/
+
+typedef enum
+{
+  TIMER2_Prescaler_0     = 0,
+  TIMER2_Prescaler_1     =                     CS20_bm,
+  TIMER2_Prescaler_8     =           CS21_bm,
+  TIMER2_Prescaler_32    =           CS21_bm | CS20_bm,
+  TIMER2_Prescaler_64    = CS22_bm,
+  TIMER2_Prescaler_128   = CS22_bm |           CS20_bm,
+  TIMER2_Prescaler_256   = CS22_bm | CS21_bm,
+  TIMER2_Prescaler_1024  = CS22_bm | CS21_bm | CS20_bm
+} TIMER2PrescalerSelect;
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+TIMER2::TIMER2(volatile uint8_t * TCNT2,
+               volatile uint8_t * TCCR2A,
+               volatile uint8_t * OCR2A,
+               volatile uint8_t * OCR2B)
+: _prescaler(0     ),
+  _TCNT2    (TCNT2 ),
+  _TCCR2A   (TCCR2A),
+  _OCR2A    (OCR2A ),
+  _OCR2B    (OCR2B )
+{
+
+}
+
+TIMER2::~TIMER2()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void TIMER2::start()
+{
+  setPrescaler_TCCR2A(_prescaler);
+}
+
+void TIMER2::stop()
+{
+  setPrescaler_TCCR2A(0);
+}
+
+void TIMER2::set(uint8_t const val)
+{
+  *_TCNT2 = val;
+}
+
+uint8_t TIMER2::get()
+{
+  return *_TCNT2;
+}
+
+void TIMER2::setCompareRegister(uint8_t const reg_sel, uint8_t const reg_val)
+{
+  switch(reg_sel)
+  {
+  case COMPARE_A: *_OCR2A = reg_val; break;
+  case COMPARE_B: *_OCR2B = reg_val; break;
+  }
+}
+
+void TIMER2::setPrescaler(uint32_t const prescaler)
+{
+  _prescaler = prescaler;
+}
+
+/**************************************************************************************
+ * PRIVATE FUNCTIONS
+ **************************************************************************************/
+
+void TIMER2::setPrescaler_TCCR2A(uint32_t const prescaler)
+{
+  *_TCCR2A &= ~(CS22_bm | CS21_bm | CS20_bm);
+
+  switch(prescaler)
+  {
+  case 0    : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_0   ); break;
+  case 1    : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_1   ); break;
+  case 8    : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_8   ); break;
+  case 32   : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_32  ); break;
+  case 64   : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_64  ); break;
+  case 128  : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_128 ); break;
+  case 256  : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_256 ); break;
+  case 1024 : *_TCCR2A |= static_cast<uint8_t>(TIMER2_Prescaler_1024); break;
+  }
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA2560 */
+
+} /* hal */
+
+} /* spectre */
