44382b12951343969e154f1f1c52eae19ebc234b
lxrobotics <cto@lxrobotics.com>
2018-03-16 07:29:36 +0100

    Adding code for storing the general purpose registers

--- src/os/avr/at90can128/task-port.c
+++ src/os/avr/at90can128/task-port.c
@@ -37,7 +37,39 @@
 void spectre_switch_task(struct task_list_t * new_task, struct task_list_t * old_task)
 {
   asm volatile(
-      "nop"   "\n\t"
-  );
+      /* Save general purpose registers R0 - R31 */
+      "push r0        \n\t"
+      "push r1        \n\t"
+      "push r2        \n\t"
+      "push r3        \n\t"
+      "push r4        \n\t"
+      "push r5        \n\t"
+      "push r6        \n\t"
+      "push r7        \n\t"
+      "push r8        \n\t"
+      "push r9        \n\t"
+      "push r10       \n\t"
+      "push r11       \n\t"
+      "push r12       \n\t"
+      "push r13       \n\t"
+      "push r14       \n\t"
+      "push r15       \n\t"
+      "push r16       \n\t"
+      "push r17       \n\t"
+      "push r18       \n\t"
+      "push r19       \n\t"
+      "push r20       \n\t"
+      "push r21       \n\t"
+      "push r22       \n\t"
+      "push r23       \n\t"
+      "push r24       \n\t"
+      "push r25       \n\t"
+      "push r26       \n\t"
+      "push r27       \n\t"
+      "push r28       \n\t"
+      "push r29       \n\t"
+      "push r30       \n\t"
+      "push r31       \n\t"
+      );
 }
 
