6b0b82c4acf4c2ffaceaea28dfef04b2e31fefe8
lxrobotics <cto@lxrobotics.com>
2018-03-20 13:20:54 +0100

    Adding nearly complete example for AS5600 driver

---
+++ examples/driver/driver-as5600-i2c-atmega328p/driver-as5600-i2c-atmega328p.cpp
@@ -0,0 +1,99 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <stdio.h>
+
+#include <avr/io.h>
+
+#include <spectre/hal/avr/ATMEGA328P/Delay.h>
+#include <spectre/hal/avr/ATMEGA328P/I2CMaster.h>
+#include <spectre/hal/avr/ATxxxx/i2c/I2CMasterBase.h>
+
+#include <spectre/driver/sensor/AS5600/AS5600.h>
+#include <spectre/driver/sensor/AS5600/AS5600_IoI2c.h>
+#include <spectre/driver/sensor/AS5600/AS5600_Control.h>
+
+/**************************************************************************************
+ * NAMESPACES
+ **************************************************************************************/
+
+using namespace spectre;
+using namespace spectre::hal;
+using namespace spectre::driver;
+
+/**************************************************************************************
+ * GLOBAL CONSTANTS
+ **************************************************************************************/
+
+static uint8_t  const AS5600_I2C_ADDR  = (0x36 << 1);
+static uint32_t const LOOP_DELAY_ms    = 1000; /* 1 s */
+
+/**************************************************************************************
+ * MAIN
+ **************************************************************************************/
+
+int main()
+{
+  /* HAL ******************************************************************************/
+
+  ATMEGA328P::I2CMaster i2c_master_atmega328p(&TWCR, &TWDR, &TWSR, &TWBR);
+  ATxxxx::I2CMasterBase i2c_master           (i2c_master_atmega328p);
+
+  i2c_master.setI2CClock(hal::interface::I2CMasterConfiguration::F_100_kHz);
+
+  ATMEGA328P::Delay     delay;
+
+  /* DRIVER ***************************************************************************/
+
+  sensor::AS5600::AS5600_IoI2c      as5600_io_i2c (AS5600_I2C_ADDR, i2c_master);
+  sensor::AS5600::AS5600_Control    as5600_control(as5600_io_i2c              );
+  sensor::AS5600::AS5600            as5600        (as5600_control             );
+
+  /* APPLICATION **********************************************************************/
+
+  uint8_t power_mode            = static_cast<uint8_t>(sensor::AS5600::interface::PowerModeSelect::PM_NORMAL                    );
+  uint8_t hysteresis            = static_cast<uint8_t>(sensor::AS5600::interface::HysteresisSelect::HYST_OFF                    );
+  uint8_t output_stage          = static_cast<uint8_t>(sensor::AS5600::interface::OutputStageSelect::OUTPUT_STAGE_REDUCED_ANALOG);
+  uint8_t pwm_frequency         = static_cast<uint8_t>(sensor::AS5600::interface::PWMFrequencySelect::PWM_FREQ_115_Hz           );
+  uint8_t slow_filter           = static_cast<uint8_t>(sensor::AS5600::interface::SlowFilterSelect::SF_4x                       );
+  uint8_t fast_filter_threshold = static_cast<uint8_t>(sensor::AS5600::interface::FastFilterThreshold::FTH_ONLY_SLOW_FILTER     );
+
+  as5600.open();
+
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_POWER_MODE,            static_cast<void *>(&power_mode           ));
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_HYSTERESIS,            static_cast<void *>(&hysteresis           ));
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_OUTPUT_STAGE,          static_cast<void *>(&output_stage         ));
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_PWM_FREQUENCY,         static_cast<void *>(&pwm_frequency        ));
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_SLOW_FILTER,           static_cast<void *>(&slow_filter          ));
+  as5600.ioctl(sensor::AS5600::IOCTL_SET_FAST_FILTER_THRESHOLD, static_cast<void *>(&fast_filter_threshold));
+
+  for(;;)
+  {
+    /* TODO Read from sensor and write to serial debug interface */
+
+    delay.delay_ms(LOOP_DELAY_ms);
+  }
+
+  as5600.close();
+
+  return 0;
+}
