38e0a0510693e9467d62726651575522b1627301
lxrobotics <cto@lxrobotics.com>
2018-03-26 10:11:48 +0200

    Adding DigitalInPin to AT90CAN32/64

---
+++ include/spectre/hal/avr/AT90CAN64/DigitalInPin.h
@@ -0,0 +1,65 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_AT90CAN64_DIGITALINPIN_H_
+#define INCLUDE_SPECTRE_HAL_AVR_AT90CAN64_DIGITALINPIN_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/hal/avr/ATxxxx/DigitalInPin.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace AT90CAN64
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class DigitalInPin : public ATxxxx::DigitalInPin
+{
+
+public:
+
+           DigitalInPin(volatile uint8_t * ddr, volatile uint8_t * port, volatile uint8_t * pin, uint8_t const in_pin_number) : ATxxxx::DigitalInPin(ddr, port, pin, in_pin_number) { }
+  virtual ~DigitalInPin() { }
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* AT90CAN64 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_AT90CAN64_DIGITALINPIN_H_ */
