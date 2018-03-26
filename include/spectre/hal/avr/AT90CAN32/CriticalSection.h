5626e65c530e6a7a9b3d0388234d73d7bf258473
lxrobotics <cto@lxrobotics.com>
2018-03-26 10:03:52 +0200

    Adding CriticalSection to AT90CAN32/64

---
+++ include/spectre/hal/avr/AT90CAN32/CriticalSection.h
@@ -0,0 +1,65 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_AT90CAN32_CRITICALSECTION_H_
+#define INCLUDE_SPECTRE_HAL_AVR_AT90CAN32_CRITICALSECTION_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/hal/avr/ATxxxx/CriticalSection.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace AT90CAN32
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class CriticalSection : public ATxxxx::CriticalSection
+{
+
+public:
+
+           CriticalSection(volatile uint8_t * sreg) : ATxxxx::CriticalSection(sreg) { }
+  virtual ~CriticalSection() { }
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* AT90CAN32 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_AT90CAN32_CRITICALSECTION_H_ */
