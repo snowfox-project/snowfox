39716ad60541e5061539e1e13d98b1315fa28a5f
lxrobotics <cto@lxrobotics.com>
2018-04-17 08:33:37 +0200

    Adding ATMEGA640/1280::DigitalOutPort

---
+++ include/spectre/hal/avr/ATMEGA1280/DigitalOutPort.h
@@ -0,0 +1,65 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_ATMEGA1280_DIGITALOUTPORT_H_
+#define INCLUDE_SPECTRE_HAL_AVR_ATMEGA1280_DIGITALOUTPORT_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/hal/avr/common/ATxxxx/DigitalOutPort.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA1280
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class DigitalOutPort : public ATxxxx::DigitalOutPort
+{
+
+public:
+
+           DigitalOutPort(volatile uint8_t * ddr, volatile uint8_t * port) : ATxxxx::DigitalOutPort(ddr, port) { }
+  virtual ~DigitalOutPort() { }
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA1280 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_ATMEGA1280_DIGITALOUTPORT_H_ */
