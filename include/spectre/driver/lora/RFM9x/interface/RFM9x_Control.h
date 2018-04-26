e1e40562de3eabd04f51dc33f6cc37d56111c583
lxrobotics <cto@lxrobotics.com>
2018-04-26 13:59:22 +0200

    Deleting RFM9x_Configuration and RFM9x_FifoControl for better readabiltiy

--- include/spectre/driver/lora/RFM9x/interface/RFM9x_Control.h
+++ include/spectre/driver/lora/RFM9x/interface/RFM9x_Control.h
@@ -23,8 +23,7 @@
  * INCLUDES
  **************************************************************************************/
 
-#include <spectre/driver/lora/RFM9x/interface/RFM9x_FifoControl.h>
-#include <spectre/driver/lora/RFM9x/interface/RFM9x_Configuration.h>
+#include <stdint.h>
 
 /**************************************************************************************
  * NAMESPACE
@@ -46,11 +45,51 @@ namespace interface
 {
 
 /**************************************************************************************
+ * DEFINES
+ **************************************************************************************/
+
+/* REG_OP_MODE ************************************************************************/
+#define RFM9x_REG_OP_MODE_LONG_RANGE_MODE_bm    (1<<7)
+#define RFM9x_REG_OP_MODE_MODULATION_TYPE_1_bm  (1<<6)
+#define RFM9x_REG_OP_MODE_MODULATION_TYPE_0_bm  (1<<5)
+#define RFM9x_REG_OP_MODE_MODE_2_bm             (1<<2)
+#define RFM9x_REG_OP_MODE_MODE_1_bm             (1<<1)
+#define RFM9x_REG_OP_MODE_MODE_0_bm             (1<<0)
+
+/**************************************************************************************
+ * TYPEDEFS
+ **************************************************************************************/
+
+enum class OperatingMode : uint8_t
+{
+  SLEEP        = 0,
+  STDBY        =                                                             RFM9x_REG_OP_MODE_MODE_0_bm,
+  FSTX         =                               RFM9x_REG_OP_MODE_MODE_1_bm,
+  TX           =                               RFM9x_REG_OP_MODE_MODE_1_bm | RFM9x_REG_OP_MODE_MODE_0_bm,
+  FSRX         = RFM9x_REG_OP_MODE_MODE_2_bm,
+  RXCONTINUOUS = RFM9x_REG_OP_MODE_MODE_2_bm |                               RFM9x_REG_OP_MODE_MODE_0_bm,
+  RXSINGLE     = RFM9x_REG_OP_MODE_MODE_2_bm | RFM9x_REG_OP_MODE_MODE_1_bm,
+  CAD          = RFM9x_REG_OP_MODE_MODE_2_bm | RFM9x_REG_OP_MODE_MODE_1_bm | RFM9x_REG_OP_MODE_MODE_0_bm
+};
+
+enum class LoRaMode : uint8_t
+{
+  FSK_OOK       = 0,
+  LoRa          = RFM9x_REG_OP_MODE_LONG_RANGE_MODE_bm
+};
+
+enum class ModulationType : uint8_t
+{
+  FSK           = 0,
+  OOK           = RFM9x_REG_OP_MODE_MODULATION_TYPE_0_bm
+};
+
+
+/**************************************************************************************
  * CLASS DECLARATION
  **************************************************************************************/
 
-class RFM9x_Control : public RFM9x_FifoControl,
-                      public RFM9x_Configuration
+class RFM9x_Control
 {
 
 public:
@@ -72,6 +111,10 @@ public:
   virtual void setLoRaMode      (LoRaMode       const lora_mode      ) = 0;
   virtual void setModulationType(ModulationType const modulation_type) = 0;
   virtual void setFrequency     (uint32_t       const freq_Hz        ) = 0;
+
+  /* Interrupt Request Handling */
+
+
 
 };
 
