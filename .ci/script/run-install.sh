bd2ca851f8674b54becf2c4bc7e2a50aca5530ec
Alexander Entinger <cto@lxrobotics.com>
2018-05-10 08:02:06 +0200

    Update run-install.sh

--- .ci/script/run-install.sh
+++ .ci/script/run-install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
+sudo apt-get update
 sudo apt-get install cppcheck
 sudo apt-get install lcov
 gem install coveralls-lcov
 lcov --version
 g++ --version
-
 sudo apt-get install cmake
 sudo apt-get install avr-libc binutils-avr gcc-avr avrdude
 
