acb63f5adf5a6a4c0c74028df91565634ab38dd2
lxrobotics <cto@lxrobotics.com>
2018-05-14 14:31:16 +0200

    Adding class RFM9x_Dio1EventCallback for handling DIO1 events

--- .cmake/driver-srcs.cmake
+++ .cmake/driver-srcs.cmake
@@ -79,6 +79,7 @@ if(${DRIVER_LORA_RFM9x} STREQUAL "yes")
     ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO0/RFM9x_onTxDoneCallback.cpp
     
     ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO1/RFM9x_Dio1Configuration.cpp
+    ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO1/RFM9x_Dio1EventCallback.cpp
     ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO1/RFM9x_onCadDetectedCallback.cpp
     ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO1/RFM9x_onFhssChangeChannelCallback.cpp
     ${DRIVER_LORA_SRC_PATH}/RFM9x/DIO1/RFM9x_onRxTimeoutCallback.cpp
---
+++ src/driver/lora/RFM9x/DIO1/RFM9x_Dio1EventCallback.cpp
@@ -0,0 +1,104 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/driver/lora/RFM9x/DIO1/RFM9x_Dio1EventCallback.h>
+
+#include <spectre/driver/lora/RFM9x/RFM9x_InterruptControl.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace driver
+{
+
+namespace lora
+{
+
+namespace RFM9x
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+RFM9x_Dio1EventCallback::RFM9x_Dio1EventCallback(interface::RFM9x_InterruptControl            & int_ctrl,
+                                                 interface::RFM9x_onRxTimeoutCallback         & on_rx_timeout_callback,
+                                                 interface::RFM9x_onFhssChangeChannelCallback & on_fhss_change_channel_callback,
+                                                 interface::RFM9x_onCadDetectedCallback       & on_cad_detected_callback)
+: _int_ctrl                       (int_ctrl                       ),
+  _on_rx_timeout_callback         (on_rx_timeout_callback         ),
+  _on_fhss_change_channel_callback(on_fhss_change_channel_callback),
+  _on_cad_detected_callback       (on_cad_detected_callback       )
+{
+
+}
+
+RFM9x_Dio1EventCallback::~RFM9x_Dio1EventCallback()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void RFM9x_Dio1EventCallback::onExternalEventCallback()
+{
+  uint8_t irq_req_flags = 0;
+
+  _int_ctrl.getIntReqFlags(&irq_req_flags);
+
+  /* RX TIMEOUT ***********************************************************************/
+  if(RFM9x_InterruptControl::isRxTimeout(irq_req_flags))
+  {
+    _on_rx_timeout_callback.onRxTimeout();
+    _int_ctrl.clearIntReqFlag(interface::InterruptRequest::RxTimeout);
+  }
+  /* FHSS CHANGE CHANNEL **************************************************************/
+  if(RFM9x_InterruptControl::isFhssChangeChannel(irq_req_flags))
+  {
+    _on_fhss_change_channel_callback.onFhssChangeChannel();
+    _int_ctrl.clearIntReqFlag(interface::InterruptRequest::FhssChangeChannel);
+  }
+  /* CAD DETECTED *********************************************************************/
+  if(RFM9x_InterruptControl::isCadDetected(irq_req_flags))
+  {
+    _on_cad_detected_callback.onCadDetected();
+    _int_ctrl.clearIntReqFlag(interface::InterruptRequest::CadDetected);
+  }
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* RFM9x */
+
+} /* lora */
+
+} /* driver */
+
+} /* spectre */
