290b28714a827ef83a9486752638d5e26bcd41b8
lxrobotics <cto@lxrobotics.com>
2018-05-24 12:28:59 +0200

    Adding empty implementation of an event group - still need to gather some ideas how to implement this

---
+++ include/spectre/os/EventGroup.h
@@ -0,0 +1,60 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_OS_EVENTGROUP_H_
+#define INCLUDE_SPECTRE_OS_EVENTGROUP_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include <spectre/os/interface/EventConsumer.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace os
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class EventGroup
+{
+
+public:
+
+   EventGroup();
+  ~EventGroup();
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* spectre */
+
+} /* os */
+
+#endif /* INCLUDE_SPECTRE_OS_EVENTGROUP_H_ */
