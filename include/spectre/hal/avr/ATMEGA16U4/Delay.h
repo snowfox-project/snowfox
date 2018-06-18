e679bb809f0ef247d333f59c429249852c1c592a
lxrobotics <cto@lxrobotics.com>
2018-06-18 08:43:24 +0200

    Adding suppor for ATMEGA13U4::CriticalSection/Delay/DigitalInOutPort/DigitalInPin/DigitalInPort/DigitalOutPin/DigitalOutPort/SpiMaster

---
+++ include/spectre/hal/avr/ATMEGA16U4/Delay.h
@@ -0,0 +1,57 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_ATMEGA16U4_DELAY_H_
+#define INCLUDE_SPECTRE_HAL_AVR_ATMEGA16U4_DELAY_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/hal/avr/common/ATxxxx/Delay.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA16U4
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+typedef ATxxxx::Delay Delay;
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA16U4 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_ATMEGA16U4_DELAY_H_ */
