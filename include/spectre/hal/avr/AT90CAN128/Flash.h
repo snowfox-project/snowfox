09efae371d99bb5b3a0dab4cef7d925d78faad04
lxrobotics <cto@lxrobotics.com>
2018-06-19 10:30:55 +0200

    Adding AT90CAN32/64/128/ATMEGA640/1280/2560/ATMEGA16U4/32U4::Flash

---
+++ include/spectre/hal/avr/AT90CAN128/Flash.h
@@ -0,0 +1,57 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_AT90CAN128_FLASH_H_
+#define INCLUDE_SPECTRE_HAL_AVR_AT90CAN128_FLASH_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/hal/avr/common/ATxxxx/Flash.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace AT90CAN128
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+typedef ATxxxx::Flash Flash;
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* AT90CAN128 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_AT90CAN128_FLASH_H_ */
