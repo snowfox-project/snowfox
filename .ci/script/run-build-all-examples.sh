07425748302cd9011e92814a51613d650ece183f
lxrobotics <cto@lxrobotics.com>
2018-06-28 13:26:50 +0200

    Adding all sensors to automatic example build

--- .ci/script/run-build-all-examples.sh
+++ .ci/script/run-build-all-examples.sh
@@ -2,39 +2,79 @@
 
 cd $(dirname $(readlink -f $0))
 
-#######################################################################################################
+##########################################################################
+# DEBUG ##################################################################
+##########################################################################
 
-echo "[debug::ATMEGA328P::serial]"
 ./run-build-single-example.sh examples/debug/debug-serial-atmega328p-uart0
 
-#######################################################################################################
 
-echo "[driver::ATMEGA328P::can::MCP2515]"
+##########################################################################
+# DRIVER #################################################################
+##########################################################################
+
+##########################################################################
+# CAN
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/can/MCP2515/driver-mcp2515-spi-atmega328p-transmitter
 
-echo "[driver::AT90CAN128::glcd::RA6963]"
+##########################################################################
+# GLCD
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/glcd/RA6963/driver-ra6963-gpio-at90can128
 
-echo "[driver::ATMEGA328P::haptic::DRV2605]"
+##########################################################################
+# HAPTIC
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/haptic/DRV2605/driver-drv2605-i2c-atmega328p
 
-echo "[driver::ATMEGA328P::ioexpander::PCA9547]"
+##########################################################################
+# IOEXPANDER
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/ioexpander/PCA9547/driver-pca9547-i2c-atmega328p
 
-echo "[driver::ATMEGA328P::lora::RFM9x]"
+##########################################################################
+# LORA
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-receiver
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-transmitter
 
-echo "[driver::ATMEGA328P::memory::PCF8570]"
+##########################################################################
+# MEMORY
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/memory/PCF8570/driver-pcf8570-i2c-atmega328p
 
-echo "[driver::AT90CAN128::serial]"
+##########################################################################
+# SENSOR
+##########################################################################
+
+./run-build-single-example.sh examples/driver/sensor/AD7151/driver-ad7151-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/AS5600/driver-as5600-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/BMG160/driver-bmg160-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/INA220/driver-ina220-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/L3GD20/driver-l3gd20-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/LIS2DSH/driver-lis2dsh-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/LIS3DSH/driver-lis3dsh-i2c-atmega328p
+./run-build-single-example.sh examples/driver/sensor/LIS3MDL/driver-lis3mdl-i2c-atmega328p
+
+##########################################################################
+# SERIAL
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/serial/uart0-at90can128
 ./run-build-single-example.sh examples/driver/serial/uart1-at90can128
-echo "[driver::ATMEGA328P::serial]"
 ./run-build-single-example.sh examples/driver/serial/uart0-atmega328p
 
-echo "[driver::ATMEGA328P::stepper::TMC260x]"
+##########################################################################
+# STEPPER
+##########################################################################
+
 ./run-build-single-example.sh examples/driver/stepper/TMC260x/driver-tmc260x-spi-atmega328p
 
-#######################################################################################################
+##########################################################################
