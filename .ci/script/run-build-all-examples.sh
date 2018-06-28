e3af0bbfa640882f03642f6024ba2319545baf79
lxrobotics <cto@lxrobotics.com>
2018-06-28 09:46:50 +0200

    Adding PCA9547 to the automatic example build

--- .ci/script/run-build-all-examples.sh
+++ .ci/script/run-build-all-examples.sh
@@ -3,6 +3,11 @@
 cd $(dirname $(readlink -f $0))
 
 echo "[ATMEGA328P]"
+echo "  [ioexpander]"
+echo "    [PCA9547]"
+./run-build-single-example.sh examples/driver/ioexpander/PCA9547/driver-pca9547-i2c-atmega328p
+echo "  [lora]"
+echo "    [RFM9x]"
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-receiver
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-transmitter
 
--- .ci/script/run-build-single-example.sh
+++ .ci/script/run-build-single-example.sh
@@ -2,9 +2,8 @@
 
 cd $(dirname $(readlink -f $0))
 
-echo "  [LIBRARY]"
 ./run-build-libspectre.sh "$1"/config.cmake
-echo "  [BINARY]"
+
 cd ../..
 mkdir -p "$1"/build
 cd "$1"/build
