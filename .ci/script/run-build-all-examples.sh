27f54d148fa5bc13b57c3a493264a0107f98134e
lxrobotics <cto@lxrobotics.com>
2018-06-28 10:30:28 +0200

    Adding debug to the automatic example build

--- .ci/script/run-build-all-examples.sh
+++ .ci/script/run-build-all-examples.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
+#######################################################################################################
+
 cd $(dirname $(readlink -f $0))
 
-echo "[ATMEGA328P::can::MCP2515]"
+#######################################################################################################
+
+echo "[debug::ATMEGA328P::serial]"
+./run-build-single-example.sh examples/debug/debug-serial-atmega328p-uart0
+
+#######################################################################################################
+
+echo "[driver::ATMEGA328P::can::MCP2515]"
 ./run-build-single-example.sh examples/driver/can/MCP2515/driver-mcp2515-spi-atmega328p-transmitter
 
-echo "[AT90CAN128::glcd::RA6963]"
+echo "[driver::AT90CAN128::glcd::RA6963]"
 ./run-build-single-example.sh examples/driver/glcd/RA6963/driver-ra6963-gpio-at90can128
 
-echo "[ATMEGA328P::haptic::DRV2605]"
+echo "[driver::ATMEGA328P::haptic::DRV2605]"
 ./run-build-single-example.sh examples/driver/haptic/DRV2605/driver-drv2605-i2c-atmega328p
 
-echo "[ATMEGA328P::ioexpander::PCA9547]"
+echo "[driver::ATMEGA328P::ioexpander::PCA9547]"
 ./run-build-single-example.sh examples/driver/ioexpander/PCA9547/driver-pca9547-i2c-atmega328p
 
-echo "[ATMEGA328P::lora::RFM9x]"
+echo "[driver::ATMEGA328P::lora::RFM9x]"
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-receiver
 ./run-build-single-example.sh examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega328p-transmitter
 
-echo "[ATMEGA328P::memory::memory]"
+echo "[driver::ATMEGA328P::memory::PCF8570]"
 ./run-build-single-example.sh examples/driver/memory/PCF8570/driver-pcf8570-i2c-atmega328p
 
-echo "[AT90CAN128::serial]"
+echo "[driver::AT90CAN128::serial]"
 ./run-build-single-example.sh examples/driver/serial/uart0-at90can128
 ./run-build-single-example.sh examples/driver/serial/uart1-at90can128
-echo "[ATMEGA328P::serial]"
+echo "[driver::ATMEGA328P::serial]"
 ./run-build-single-example.sh examples/driver/serial/uart0-atmega328p
 
-echo "[ATMEGA328P::stepper::TMC260x]"
+echo "[driver::ATMEGA328P::stepper::TMC260x]"
 ./run-build-single-example.sh examples/driver/stepper/TMC260x/driver-tmc260x-spi-atmega328p
+
+#######################################################################################################
