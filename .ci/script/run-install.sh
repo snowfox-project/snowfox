cebf3398fdb14167207983301915ee3c54e77aa2
Alexander Entinger <cto@lxrobotics.com>
2018-07-02 10:56:05 +0200

    Specifying avr-gcc version

--- .ci/script/run-install.sh
+++ .ci/script/run-install.sh
@@ -6,7 +6,10 @@ gem install coveralls-lcov
 lcov --version
 g++ --version
 sudo apt-get install cmake
-sudo apt-get install avr-libc binutils-avr gcc-avr avrdude
+sudo apt-get install avr-libc=1:1.8.0+Atmel3.5.0-1
+sudo apt-get install binutils-avr=2.25+Atmel3.5.0-2
+sudo apt-get install gcc-avr=1:4.9.2+Atmel3.5.0-1
+sudo apt-get install avrdude
 
 
 
