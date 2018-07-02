8194496eed9af66b594075e41745a56e38984fd9
Alexander Entinger <cto@lxrobotics.com>
2018-07-02 12:37:38 +0200

    Update run-install.sh

--- .ci/script/run-install.sh
+++ .ci/script/run-install.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-travis_retry() {
-  local result=0
-  local count=1
-  while [ $count -le 3 ]; do
-    [ $result -ne 0 ] && {
-      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
-    }
-    "$@"
-    result=$?
-    [ $result -eq 0 ] && break
-    count=$(($count + 1))
-    sleep 1
-  done
+#travis_retry() {
+#  local result=0
+#  local count=1
+#  while [ $count -le 3 ]; do
+#    [ $result -ne 0 ] && {
+#      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+#    }
+#    "$@"
+#    result=$?
+#    [ $result -eq 0 ] && break
+#    count=$(($count + 1))
+#    sleep 1
+#  done
+#
+#  [ $count -gt 3 ] && {
+#    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+#  }
+#
+#  return $result
+#}
 
-  [ $count -gt 3 ] && {
-    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
-  }
-
-  return $result
-}
-
-WGET="travis_retry wget --continue --tries=20 --waitretry=10 --retry-connrefused --no-dns-cache --timeout 300"
+#WGET="travis_retry wget --continue --tries=20 --waitretry=10 --retry-connrefused --no-dns-cache --timeout 300"
 
 sudo apt-get update
 
@@ -32,9 +32,10 @@ lcov --version
 g++ --version
 
 sudo apt-get install cmake
+sudo apt-get install avr-libc binutils-avr gcc-avr avrdude
 
-$WGET http://atiselsts.github.io/resources/avr-gcc-4.9.2-compiled.tar.bz2
-tar xjf avr-gcc*.tar.bz2 -C /tmp/
-sudo cp -f -r /tmp/avr-gcc/* /usr/local/
-rm -rf /tmp/avr-gcc avr-gcc*.tar.bz2
+#$WGET http://atiselsts.github.io/resources/avr-gcc-4.9.2-compiled.tar.bz2
+#tar xjf avr-gcc*.tar.bz2 -C /tmp/
+#sudo cp -f -r /tmp/avr-gcc/* /usr/local/
+#rm -rf /tmp/avr-gcc avr-gcc*.tar.bz2
 avr-gcc --version
