1f6fe6cf873ca204232b3f802ebfe661d9dadafd
Alexander Entinger <cto@lxrobotics.com>
2018-07-02 11:16:26 +0200

    Now travis_retry should work ...
    see https://twitter.com/plexus/status/499194992632811520?lang=de

--- .ci/script/run-install.sh
+++ .ci/script/run-install.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-#source travis_retry.sh
-#WGET="travis_retry wget --continue --tries=20 --waitretry=10 --retry-connrefused --no-dns-cache --timeout 300"
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    "$@"
+    result=$?
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
+
+WGET="travis_retry wget --continue --tries=20 --waitretry=10 --retry-connrefused --no-dns-cache --timeout 300"
 
 sudo apt-get update
 
@@ -13,7 +33,7 @@ g++ --version
 
 sudo apt-get install cmake
 
-wget http://atiselsts.github.io/resources/avr-gcc-4.9.2-compiled.tar.bz2
+$WGET http://atiselsts.github.io/resources/avr-gcc-4.9.2-compiled.tar.bz2
 tar xjf avr-gcc*.tar.bz2 -C /tmp/
 sudo cp -f -r /tmp/avr-gcc/* /usr/local/
 rm -rf /tmp/avr-gcc avr-gcc*.tar.bz2
