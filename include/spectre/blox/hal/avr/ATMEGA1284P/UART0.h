192934a88dc0649355a3da1223d9a435bbbc079f
lxrobotics <cto@lxrobotics.com>
2018-07-16 08:35:57 +0200

    Adding blox::ATMEGA1284P::UART0

---
+++ include/spectre/blox/hal/avr/ATMEGA1284P/UART0.h
@@ -0,0 +1,81 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_BLOX_HAL_AVR_ATMEGA1284P_UART0_H_
+#define INCLUDE_SPECTRE_BLOX_HAL_AVR_ATMEGA1284P_UART0_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/hal/avr/ATMEGA1284P/UART0.h>
+
+#include <spectre/hal/interface/interrupt/InterruptControllerAssembly.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace blox
+{
+
+namespace ATMEGA1284P
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+class UART0
+{
+
+public:
+
+  UART0(volatile uint8_t                                  * udr0,
+        volatile uint8_t                                  * ucsr0a,
+        volatile uint8_t                                  * ucsr0b,
+        volatile uint8_t                                  * ucsr0c,
+        volatile uint16_t                                 * ubrr0,
+        hal::interface::InterruptController               & int_ctrl,
+        hal::interface::InterruptControllerAssembly       & int_ctrl_assembly,
+        uint32_t                                    const   f_cpu);
+
+  hal::ATMEGA1284P::UART0 & operator () () { return _uart0; }
+
+private:
+
+  hal::ATMEGA1284P::UART0                                _uart0;
+  hal::ATMEGA1284P::UART0_TransmitRegisterEmptyCallback  _uart0_uart_data_register_empty_callback;
+  hal::ATMEGA1284P::UART0_ReceiveCompleteCallback        _uart0_receive_complete_callback;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA1284P */
+
+} /* blox */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_BLOX_HAL_AVR_ATMEGA1284P_UART0_H_ */
