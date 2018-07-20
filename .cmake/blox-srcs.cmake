8cd97c290d06070228a1a2bbf8d1cfd94490f003
lxrobotics <cto@lxrobotics.com>
2018-07-20 08:52:02 +0200

    Refactoring blox-srcs.cmake for better readability

--- .cmake/blox-srcs.cmake
+++ .cmake/blox-srcs.cmake
@@ -30,64 +30,65 @@ set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLO
 # AVR ####################################################################
 ##########################################################################
 
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH src/blox/hal/avr)
-
-# ATMEGA1284P ############################################################
-
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA1284P_SRCS 
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA1284P/UART0.cpp
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA1284P/UART1.cpp
-)
-
-# ATMEGA164P #############################################################
-
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA164P_SRCS 
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA164P/UART0.cpp
-)
-
-# ATMEGA324P #############################################################
-
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA324P_SRCS 
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA324P/UART0.cpp
-)
-
-# ATMEGA328P #############################################################
-
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA328P_SRCS 
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA328P/UART0.cpp
-)
-
-# ATMEGA644P #############################################################
-
-set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA644P_SRCS 
-  ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA644P/UART0.cpp
-)
+if(${MCU_ARCH} STREQUAL "avr")
 
 ##########################################################################
 
-if(${MCU_ARCH} STREQUAL "avr")
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH src/blox/hal/avr)
+
+  # ATMEGA1284P ##########################################################
+
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA1284P_SRCS 
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA1284P/UART0.cpp
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA1284P/UART1.cpp
+  )
 
   if(${MCU_TYPE} STREQUAL "atmega1284p")
     set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA1284P_SRCS})  
   endif()
+
+  # ATMEGA164P ###########################################################
+
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA164P_SRCS 
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA164P/UART0.cpp
+  )
   
   if(${MCU_TYPE} STREQUAL "atmega164p")
     set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA164P_SRCS})  
   endif()
+  
+  # ATMEGA324P ###########################################################
 
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA324P_SRCS 
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA324P/UART0.cpp
+  )
+  
   if(${MCU_TYPE} STREQUAL "atmega324p")
     set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA324P_SRCS})  
   endif()
 
+  # ATMEGA328P ###########################################################
+
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA328P_SRCS 
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA328P/UART0.cpp
+  )
+  
   if(${MCU_TYPE} STREQUAL "atmega328p")
     set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA328P_SRCS})  
   endif()
+  
+  # ATMEGA644P ###########################################################
 
+  set(SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA644P_SRCS 
+    ${SPECTRE_LIBRARY_BLOX_HAL_AVR_PATH}/ATMEGA644P/UART0.cpp
+  )
+  
   if(${MCU_TYPE} STREQUAL "atmega644p")
     set(SPECTRE_LIBRARY_BLOX_SRCS ${SPECTRE_LIBRARY_BLOX_SRCS} ${SPECTRE_LIBRARY_BLOX_HAL_AVR_ATMEGA644P_SRCS})  
   endif()
+  
+##########################################################################
 
 endif()
 
 ##########################################################################
-
