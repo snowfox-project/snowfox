942e9eb4270b9609b9956b2f9adafd18d1dca679
lxrobotics <cto@lxrobotics.com>
2018-08-03 08:58:26 +0200

    Example now complete for reading all RFM9x registers from the Moteino Mega USB

--- examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega1284p-debug-moteino-mega-usb/driver-rfm9x-spi-atmega1284p-debug-moteino-mega-usb.cpp
+++ examples/driver/lora/RFM9x/driver-rfm9x-spi-atmega1284p-debug-moteino-mega-usb/driver-rfm9x-spi-atmega1284p-debug-moteino-mega-usb.cpp
@@ -29,12 +29,19 @@
 
 #include <avr/io.h>
 
+#include <spectre/hal/avr/ATMEGA1284P/Flash.h>
+#include <spectre/hal/avr/ATMEGA1284P/SpiMaster.h>
+#include <spectre/hal/avr/ATMEGA1284P/DigitalInPin.h>
+#include <spectre/hal/avr/ATMEGA1284P/DigitalOutPin.h>
 #include <spectre/hal/avr/ATMEGA1284P/CriticalSection.h>
 #include <spectre/hal/avr/ATMEGA1284P/InterruptController.h>
 
 #include <spectre/blox/hal/avr/ATMEGA1284P/UART0.h>
 
 #include <spectre/blox/driver/serial/SerialUart.h>
+
+#include <spectre/driver/lora/RFM9x/RFM9x_Debug.h>
+#include <spectre/driver/lora/RFM9x/RFM9x_IoSpi.h>
 
 #include <spectre/debug/serial/DebugSerial.h>
 
@@ -50,8 +57,12 @@ using namespace spectre::driver;
  * CONSTANTS
  **************************************************************************************/
 
-static uint16_t const UART_RX_BUFFER_SIZE =  0;
-static uint16_t const UART_TX_BUFFER_SIZE = 16;
+static uint16_t                    const UART_RX_BUFFER_SIZE =  0;
+static uint16_t                    const UART_TX_BUFFER_SIZE = 16;
+
+static hal::interface::SpiMode     const RFM9x_SPI_MODE              = hal::interface::SpiMode::MODE_0;
+static hal::interface::SpiBitOrder const RFM9x_SPI_BIT_ORDER         = hal::interface::SpiBitOrder::MSB_FIRST;
+static uint32_t                    const RFM9x_SPI_PRESCALER         = 16; /* Moteino Mega USB CLK = 16 MHz -> SPI Clk = 1 MHz */
 
 /**************************************************************************************
  * MAIN
@@ -59,36 +70,62 @@ static uint16_t const UART_TX_BUFFER_SIZE = 16;
 
 int main()
 {
-  /* HAL ******************************************************************************/
+  /************************************************************************************
+   * HAL
+   ************************************************************************************/
 
+  ATMEGA1284P::Flash               flash;
   ATMEGA1284P::InterruptController int_ctrl(&EIMSK, &PCICR, &WDTCSR, &TIMSK2, &TIMSK1, &TIMSK0, &SPCR, &UCSR0B, &ACSR, &ADCSRA, &EECR, &TWCR, &SPMCSR, &UCSR1B);
   ATMEGA1284P::CriticalSection     crit_sec(&SREG);
-
   blox::ATMEGA1284P::UART0         uart0   (&UDR0, &UCSR0A, &UCSR0B, &UCSR0C, &UBRR0, int_ctrl, F_CPU);
 
 
-  /* DRIVER ***************************************************************************/
+  /* SPI/CS for RFM95 *****************************************************************/
+  ATMEGA1284P::DigitalOutPin rfm9x_cs  (&DDRB, &PORTB,        4);     /* CS   = D4 = PB4 */
+  ATMEGA1284P::DigitalOutPin rfm9x_sck (&DDRB, &PORTB,        7);     /* SCK  = D7 = PB7 */
+  ATMEGA1284P::DigitalInPin  rfm9x_miso(&DDRB, &PORTB, &PINB, 6);     /* MISO = D6 = PB6 */
+  ATMEGA1284P::DigitalOutPin rfm9x_mosi(&DDRB, &PORTB,        5);     /* MOSI = D5 = PB5 */
 
+  rfm9x_cs.set();
+  rfm9x_miso.setPullUpMode(hal::interface::PullUpMode::PULL_UP);
+
+  ATMEGA1284P::SpiMaster     spi_master(&SPCR, &SPSR, &SPDR);
+
+  spi_master.setSpiMode     (RFM9x_SPI_MODE     );
+  spi_master.setSpiBitOrder (RFM9x_SPI_BIT_ORDER);
+  spi_master.setSpiPrescaler(RFM9x_SPI_PRESCALER);
+
+
+  /************************************************************************************
+   * DRIVER
+   ************************************************************************************/
+
+  /* SERIAL ***************************************************************************/
   blox::SerialUart serial(crit_sec, uart0(), UART_RX_BUFFER_SIZE, UART_TX_BUFFER_SIZE);
 
   uint8_t baud_rate = static_cast<uint8_t>(serial::interface::SerialBaudRate::B115200);
   uint8_t parity    = static_cast<uint8_t>(serial::interface::SerialParity::None     );
   uint8_t stop_bit  = static_cast<uint8_t>(serial::interface::SerialStopBit::_1      );
 
-  serial().open();
   serial().ioctl(serial::IOCTL_SET_BAUDRATE, static_cast<void *>(&baud_rate));
   serial().ioctl(serial::IOCTL_SET_PARITY,   static_cast<void *>(&parity   ));
   serial().ioctl(serial::IOCTL_SET_STOPBIT,  static_cast<void *>(&stop_bit ));
 
+  debug::DebugSerial debug_serial(serial());
+
+  /* RFM95 ****************************************************************************/
+  lora::RFM9x::RFM9x_IoSpi rfm9x_spi(spi_master, rfm9x_cs);
+
 
   /* GLOBAL INTERRUPT *****************************************************************/
-
   int_ctrl.enableInterrupt(ATMEGA164P_324P_644P_1284P::toIntNum(ATMEGA1284P::Interrupt::GLOBAL));
 
 
-  /* APPLICATION **********************************************************************/
+  /************************************************************************************
+   * APPLICATION
+   ************************************************************************************/
 
-  debug::DebugSerial debug_serial(serial());
+  lora::RFM9x::RFM9x_Debug::debug_dumpAllRegs(debug_serial, flash, rfm9x_spi);
 
   for(;;)
   {
