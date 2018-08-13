8d86a1fe73e32faab8882727ffb1130651b238d6
lxrobotics <cto@lxrobotics.com>
2018-08-13 07:04:24 +0200

    Adding blox::AT90CAN32/64/128::ExternalInterruptController

---
+++ include/spectre/blox/hal/avr/AT90CAN64/ExternalInterruptController.h
@@ -0,0 +1,57 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_BLOX_HAL_AVR_AT90CAN64_EXTERNALINTERRUPTCONTROLLER_H_
+#define INCLUDE_SPECTRE_BLOX_HAL_AVR_AT90CAN64_EXTERNALINTERRUPTCONTROLLER_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/blox/hal/avr/common/AT90CAN32_64_128/ExternalInterruptController.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace blox
+{
+
+namespace AT90CAN64
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+typedef AT90CAN32_64_128::ExternalInterruptController ExternalInterruptController;
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* AT90CAN64 */
+
+} /* blox */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_BLOX_HAL_AVR_AT90CAN64_EXTERNALINTERRUPTCONTROLLER_H_ */
