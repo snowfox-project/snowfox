311aa80855fff1af7c1337880229a3bfb46796a6
lxrobotics <cto@lxrobotics.com>
2018-08-22 14:32:08 +0200

    Removing unncessary examples and simplifying compile process

--- .ci/script/run-build-example.sh
+++ .ci/script/run-build-example.sh
@@ -4,15 +4,12 @@ cd $(dirname $(readlink -f $0))
 
 #######################################################################################################
 
-./run-build-libspectre.sh "$1"/config.cmake
-
-#######################################################################################################
-
 cd ../..
-rm -rf "$1"/build
-mkdir -p "$1"/build
-cd "$1"/build
-cmake ..
-make -j4
+
+cp "$1"/config.cmake config.cmake
+
+(rm -rf build && mkdir build && cd build; cmake ..; make -j4; sudo make install)
+(cd "$1"; rm -rf build && mkdir -p build && cd build; cmake ..; make -j4)
+(cd build; sudo make uninstall)
 
 #######################################################################################################
