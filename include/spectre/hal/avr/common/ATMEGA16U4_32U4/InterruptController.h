d0b1dbbea4f4d33139073ec9a97b71da9619fd20
lxrobotics <cto@lxrobotics.com>
2018-08-23 17:02:23 +0200

    Adding skeleton module for hal::ATMEGA16U4_32U4::InterruptController

---
+++ include/spectre/hal/avr/common/ATMEGA16U4_32U4/InterruptController.h
@@ -0,0 +1,152 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_HAL_AVR_COMMON_ATMEGA16U4_32U4_INTERRUPTCONTROLLER_H_
+#define INCLUDE_SPECTRE_HAL_AVR_COMMON_ATMEGA16U4_32U4_INTERRUPTCONTROLLER_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/hal/interface/interrupt/InterruptController.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA16U4_32U4
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+enum class Interrupt : uint8_t
+{
+  EXTERNAL_INT0                   =  0,
+  EXTERNAL_INT1                   =  1,
+  EXTERNAL_INT2                   =  2,
+  EXTERNAL_INT3                   =  3,
+  EXTERNAL_INT6                   =  4,
+  PIN_CHANGE_INT0                 =  5,
+  USB_GENERAL                     =  6,
+  USB_ENDPOINT                    =  7,
+  WATCHDOG_TIMER                  =  8,
+  TIMER1_CAPTURE                  =  9,
+  TIMER1_COMPARE_A                = 10,
+  TIMER1_COMPARE_B                = 11,
+  TIMER1_COMPARE_C                = 12,
+  TIMER1_OVERFLOW                 = 13,
+  TIMER0_COMPARE_A                = 14,
+  TIMER0_COMPARE_B                = 15,
+  TIMER0_OVERFLOW                 = 16,
+  SPI_SERIAL_TRANSFER_COMPLETE    = 17,
+  USART1_RECEIVE_COMPLETE         = 18,
+  USART1_UART_DATA_REGISTER_EMPTY = 19,
+  USART1_TRANSMIT_COMPLETE        = 20,
+  ANALOG_COMPARATOR               = 21,
+  ANALOG_DIGITAL_CONVERTER        = 22,
+  EEPROM_READY                    = 23,
+  TIMER3_CAPTURE                  = 24,
+  TIMER3_COMPARE_A                = 25,
+  TIMER3_COMPARE_B                = 26,
+  TIMER3_COMPARE_C                = 27,
+  TIMER3_OVERFLOW                 = 28,
+  TWO_WIRE_INT                    = 29,
+  SPM_READY                       = 30,
+  TIMER4_CAPTURE                  = 31,
+  TIMER4_COMPARE_A                = 32,
+  TIMER4_COMPARE_B                = 33,
+  TIMER4_COMPARE_C                = 34,
+  TIMER4_OVERFLOW                 = 35,
+  TIMER4_FAULT_PROTECTION         = 36,
+  GLOBAL                          = 37,
+  INVALID                         = interface::InterruptController::INVALID_INT_NUM
+};
+
+enum class InterruptServiceRoutine : uint8_t
+{
+  EXTERNAL_INT0                   =  0,
+  EXTERNAL_INT1                   =  1,
+  EXTERNAL_INT2                   =  2,
+  EXTERNAL_INT3                   =  3,
+  EXTERNAL_INT6                   =  4
+  /* TODO */
+};
+
+/**************************************************************************************
+ * CONSTEXPR FUNCTIONS
+ **************************************************************************************/
+
+constexpr uint8_t toIntNum(Interrupt const interrupt)
+{
+  return static_cast<uint8_t>(interrupt);
+}
+
+constexpr uint8_t toIsrNum(InterruptServiceRoutine const interrupt_service_routine)
+{
+  return static_cast<uint8_t>(interrupt_service_routine);
+}
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class InterruptController : public interface::InterruptController
+{
+
+public:
+
+           InterruptController(volatile uint8_t * eimsk);
+  virtual ~InterruptController();
+
+
+  /* Interrupt Controller Interface */
+
+  virtual void enableInterrupt (uint8_t const int_num) override;
+  virtual void disableInterrupt(uint8_t const int_num) override;
+
+
+  /* Interrupt Controller Assembly Interface */
+
+  virtual void registerInterruptCallback(uint8_t const isr_num, interface::InterruptCallback * interrupt_callback) override;
+
+
+private:
+
+  volatile uint8_t * _EIMSK;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA16U4_32U4 */
+
+} /* hal */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_HAL_AVR_COMMON_ATMEGA16U4_32U4_INTERRUPTCONTROLLER_H_ */
---
+++ src/hal/avr/common/ATMEGA16U4_32U4/InterruptController.cpp
@@ -0,0 +1,179 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/hal/avr/common/ATMEGA16U4_32U4/InterruptController.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace hal
+{
+
+namespace ATMEGA16U4_32U4
+{
+
+/**************************************************************************************
+ * DEFINES
+ **************************************************************************************/
+
+/* EIMSK */
+#define INT6_bm   (1<<6)
+#define INT3_bm   (1<<3)
+#define INT2_bm   (1<<2)
+#define INT1_bm   (1<<1)
+#define INT0_bm   (1<<0)
+
+/**************************************************************************************
+ * GLOBAL VARIABLES
+ **************************************************************************************/
+
+static interface::InterruptCallback * isr_external_int0                   = 0,
+                                    * isr_external_int1                   = 0,
+                                    * isr_external_int2                   = 0,
+                                    * isr_external_int3                   = 0,
+                                    * isr_external_int6                   = 0;
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+InterruptController::InterruptController(volatile uint8_t * eimsk)
+: _EIMSK (eimsk )
+{
+
+}
+
+InterruptController::~InterruptController()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void InterruptController::enableInterrupt(uint8_t const int_num)
+{
+  switch(int_num)
+  {
+  case toIntNum(Interrupt::EXTERNAL_INT0                  ): *_EIMSK   |= INT0_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT1                  ): *_EIMSK   |= INT1_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT2                  ): *_EIMSK   |= INT2_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT3                  ): *_EIMSK   |= INT3_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT6                  ): *_EIMSK   |= INT6_bm;   break;
+#if defined(MCU_ARCH_avr)
+  case toIntNum(Interrupt::GLOBAL                         ): asm volatile("sei");    break;
+#endif
+  case toIntNum(Interrupt::INVALID                        ): /* DO NOTHING */        break;
+  }
+}
+
+void InterruptController::disableInterrupt(uint8_t const int_num)
+{
+  switch(int_num)
+  {
+  case toIntNum(Interrupt::EXTERNAL_INT0                  ): *_EIMSK   &= ~INT0_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT1                  ): *_EIMSK   &= ~INT1_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT2                  ): *_EIMSK   &= ~INT2_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT3                  ): *_EIMSK   &= ~INT3_bm;   break;
+  case toIntNum(Interrupt::EXTERNAL_INT6                  ): *_EIMSK   &= ~INT6_bm;   break;
+#if defined(MCU_ARCH_avr)
+  case toIntNum(Interrupt::GLOBAL                         ): asm volatile("cli");     break;
+#endif
+  }
+}
+
+void InterruptController::registerInterruptCallback(uint8_t const isr_num, interface::InterruptCallback * interrupt_callback)
+{
+  switch(isr_num)
+  {
+  case toIsrNum(InterruptServiceRoutine::EXTERNAL_INT0                  ): isr_external_int0                   = interrupt_callback; break;
+  case toIsrNum(InterruptServiceRoutine::EXTERNAL_INT1                  ): isr_external_int1                   = interrupt_callback; break;
+  case toIsrNum(InterruptServiceRoutine::EXTERNAL_INT2                  ): isr_external_int2                   = interrupt_callback; break;
+  case toIsrNum(InterruptServiceRoutine::EXTERNAL_INT3                  ): isr_external_int3                   = interrupt_callback; break;
+  case toIsrNum(InterruptServiceRoutine::EXTERNAL_INT6                  ): isr_external_int6                   = interrupt_callback; break;
+  }
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* ATMEGA16U4_32U4 */
+
+} /* hal */
+
+} /* spectre */
+
+/**************************************************************************************
+ * INTERRUPT SERVICE ROUTINES
+ **************************************************************************************/
+
+#if defined(MCU_ARCH_avr) && ( defined(MCU_TYPE_atmega32u4) || defined(MCU_TYPE_atmega16u4) )
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+/**************************************************************************************
+ * NAMESPACES
+ **************************************************************************************/
+
+using namespace spectre::hal::ATMEGA16U4_32U4;
+
+/**************************************************************************************
+ * INTERRUPT SERVICE ROUTINES
+ **************************************************************************************/
+
+ISR(INT0_vect)
+{
+  if(isr_external_int0) isr_external_int0->interruptServiceRoutine();
+}
+
+ISR(INT1_vect)
+{
+  if(isr_external_int1) isr_external_int1->interruptServiceRoutine();
+}
+
+ISR(INT2_vect)
+{
+  if(isr_external_int2) isr_external_int2->interruptServiceRoutine();
+}
+
+ISR(INT3_vect)
+{
+  if(isr_external_int3) isr_external_int3->interruptServiceRoutine();
+}
+
+ISR(INT6_vect)
+{
+  if(isr_external_int6) isr_external_int6->interruptServiceRoutine();
+}
+
+#endif /* defined(MCU_ARCH_avr) && ( defined(MCU_TYPE_atmega32u4) || defined(MCU_TYPE_atmega16u4) ) */
