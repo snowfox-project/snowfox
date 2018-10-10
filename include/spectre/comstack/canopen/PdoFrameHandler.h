d6245451ce248131dce70692c3caeb48e77c5cb2
lxrobotics <cto@lxrobotics.com>
2018-10-10 14:16:18 +0200

    Adding skeleton implementations for various can open frame handlers

---
+++ include/spectre/comstack/canopen/PdoFrameHandler.h
@@ -0,0 +1,69 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_COMSTACK_CANOPEN_PDOFRAMEHANDLER_H_
+#define INCLUDE_SPECTRE_COMSTACK_CANOPEN_PDOFRAMEHANDLER_H_
+
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/comstack/canopen/interface/FrameHandler.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace comstack
+{
+
+namespace canopen
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class PdoFrameHandler : public interface::FrameHandler
+{
+
+public:
+
+           PdoFrameHandler();
+  virtual ~PdoFrameHandler();
+
+
+  virtual void onFrameReceived(CanFrame const & frame) override;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* canopen */
+
+} /* comstack */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_COMSTACK_CANOPEN_PDOFRAMEHANDLER_H_ */
---
+++ src/comstack/canopen/SyncFrameHandler.cpp
@@ -0,0 +1,69 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/comstack/canopen/SyncFrameHandler.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace comstack
+{
+
+namespace canopen
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+SyncFrameHandler::SyncFrameHandler()
+{
+
+}
+
+SyncFrameHandler::~SyncFrameHandler()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void SyncFrameHandler::onFrameReceived(CanFrame const & frame)
+{
+  /* TODO */
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* canopen */
+
+} /* comstack */
+
+} /* spectre */
