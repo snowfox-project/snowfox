5f5ec935ac1ed93e17109fb3f79fa647b0a04e7d
lxrobotics <cto@lxrobotics.com>
2018-10-19 10:57:40 +0200

    Starting to populate object dictionary with some first objects

---
+++ include/spectre/comstack/canopen/ObjectDictionary.h
@@ -0,0 +1,87 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_COMSTACK_CANOPEN_OBJECTDICTIONARY_H_
+#define INCLUDE_SPECTRE_COMSTACK_CANOPEN_OBJECTDICTIONARY_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <spectre/comstack/canopen/ObjectDictionaryEntry.hpp>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace comstack
+{
+
+namespace canopen
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+typedef struct
+{
+  uint32_t device_type;
+} ObjectDictionaryConfiguration;
+
+/**************************************************************************************
+ * CONSTANT
+ **************************************************************************************/
+
+static ObjectDictionaryConfiguration const DEFAULT_OD_CONFIG =
+{
+  0 /* device_type */
+};
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class ObjectDictionary
+{
+
+public:
+
+  ObjectDictionary(ObjectDictionaryConfiguration const & obj_dict_config);
+
+
+private:
+
+  ObjectDictionaryEntry<uint32_t> _device_type;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* canopen */
+
+} /* comstack */
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_COMSTACK_CANOPEN_OBJECTDICTIONARY_H_ */
