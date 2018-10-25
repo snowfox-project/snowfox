3450c1fc1a6452f315e411660474be2aba88aa2a
lxrobotics <cto@lxrobotics.com>
2018-10-25 11:08:28 +0200

    Making the compilation of the canopen library compilable via config file

--- .cmake/comstack-srcs.cmake
+++ .cmake/comstack-srcs.cmake
@@ -8,27 +8,21 @@
 
 set(COMSTACK_CANOPEN_SRC_PATH src/comstack/canopen)
 
-set(SPECTRE_LIBRARY_COMSTACK_CANOPEN_SRCS
-  ${COMSTACK_CANOPEN_SRC_PATH}/util/CanOpenFrameUtil.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/nmt/NmtFrameHandler.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/nodeguard/NodeGuardFrameHandler.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/objdict/ObjectDictionary.cpp
-  ${COMSTACK_CANOPEN_SRC_PATH}/objdict/ObjectDictionaryBuilder.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/pdo/PdoFrameHandler.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/sdo/SdoFrameHandler.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/sync/SyncFrameHandler.cpp
-  
-  ${COMSTACK_CANOPEN_SRC_PATH}/CanOpenStack.cpp
-  ${COMSTACK_CANOPEN_SRC_PATH}/FrameDispatcher.cpp
-  ${COMSTACK_CANOPEN_SRC_PATH}/UnkownFrameHandler.cpp
-)
-
-set(SPECTRE_LIBRARY_COMSTACK_SRCS ${SPECTRE_LIBRARY_COMSTACK_SRCS} ${SPECTRE_LIBRARY_COMSTACK_CANOPEN_SRCS})
+if(${COMSTACK_CANOPEN} STREQUAL "yes")
+  set(SPECTRE_LIBRARY_COMSTACK_CANOPEN_SRCS
+    ${COMSTACK_CANOPEN_SRC_PATH}/util/CanOpenFrameUtil.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/nmt/NmtFrameHandler.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/nodeguard/NodeGuardFrameHandler.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/objdict/ObjectDictionary.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/objdict/ObjectDictionaryBuilder.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/pdo/PdoFrameHandler.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/sdo/SdoFrameHandler.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/sync/SyncFrameHandler.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/CanOpenStack.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/FrameDispatcher.cpp
+    ${COMSTACK_CANOPEN_SRC_PATH}/UnkownFrameHandler.cpp
+  )
+  set(SPECTRE_LIBRARY_COMSTACK_SRCS ${SPECTRE_LIBRARY_COMSTACK_SRCS} ${SPECTRE_LIBRARY_COMSTACK_CANOPEN_SRCS})
+endif()
 
 ##########################################################################
