28b8b0e0dbb5b6576e29a42d37007a2baeee445f
lxrobotics <cto@lxrobotics.com>
2018-10-29 14:29:20 +0100

    Fixing test cases to work with dynamically allocated obj dict entries

--- test/src/comstack/canopen/test_ObjectDictionary.cpp
+++ test/src/comstack/canopen/test_ObjectDictionary.cpp
@@ -52,26 +52,26 @@ SCENARIO("A ObjectDictionary object is constructed", "[comstack::canopen::Object
 
   THEN("When requesting a UINT8 entry a empty entry object should be returned")
   {
-    REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT8 (0x0001, 0x01)) == true);
+    REQUIRE(obj_dict.get_UINT8 (0x0001, 0x01) == 0);
   }
   THEN("When requesting a UINT16 entry a empty entry object should be returned")
   {
-    REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT16(0x0001, 0x01)) == true);
+    REQUIRE(obj_dict.get_UINT16(0x0001, 0x01) == 0);
   }
   THEN("When requesting a UINT32 entry a empty entry object should be returned")
   {
-    REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT32(0x0001, 0x01)) == true);
+    REQUIRE(obj_dict.get_UINT32(0x0001, 0x01) == 0);
   }
   THEN("When requesting a STRING entry a empty entry object should be returned")
   {
-    REQUIRE(obj_dict.isEmpty(obj_dict.get_STRING(0x0001, 0x01)) == true);
+    REQUIRE(obj_dict.get_STRING(0x0001, 0x01) == 0);
   }
 
   /************************************************************************************/
 
   WHEN("A UINT8 obj dict entry is added")
   {
-    ObjectDictionaryEntry<uint8_t> entry(0x1001, 0x01, 0xFF, ObjectDictionaryAccess::ReadWrite, 0);
+    ObjectDictionaryEntry<uint8_t> * entry = new ObjectDictionaryEntry<uint8_t>(0x1001, 0x01, 0xFF, ObjectDictionaryAccess::ReadWrite, 0);
 
     obj_dict.add(entry);
 
@@ -81,15 +81,17 @@ SCENARIO("A ObjectDictionary object is constructed", "[comstack::canopen::Object
     }
     THEN("When requesting a not added UINT8 entry a empty entry object should be returned")
     {
-      REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT8 (0x0001, 0x01)) == true);
+      REQUIRE(obj_dict.get_UINT8 (0x0001, 0x01) == 0);
     }
+
+    delete entry; entry = 0;
   }
 
   /************************************************************************************/
 
   WHEN("A UINT16 obj dict entry is added")
   {
-    ObjectDictionaryEntry<uint16_t> entry(0x1001, 0x01, 0xCAFE, ObjectDictionaryAccess::ReadWrite, 0);
+    ObjectDictionaryEntry<uint16_t> * entry = new ObjectDictionaryEntry<uint16_t>(0x1001, 0x01, 0xCAFE, ObjectDictionaryAccess::ReadWrite, 0);
 
     obj_dict.add(entry);
 
@@ -99,15 +101,17 @@ SCENARIO("A ObjectDictionary object is constructed", "[comstack::canopen::Object
     }
     THEN("When requesting a not added UINT16 entry a empty entry object should be returned")
     {
-      REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT16(0x0001, 0x01)) == true);
+      REQUIRE(obj_dict.get_UINT16(0x0001, 0x01) == 0);
     }
+
+    delete entry; entry = 0;
   }
 
   /************************************************************************************/
 
   WHEN("A UINT32 obj dict entry is added")
   {
-    ObjectDictionaryEntry<uint32_t> entry(0x1001, 0x01, 0xCAFFEE, ObjectDictionaryAccess::ReadWrite, 0);
+    ObjectDictionaryEntry<uint32_t> * entry = new ObjectDictionaryEntry<uint32_t>(0x1001, 0x01, 0xCAFFEE, ObjectDictionaryAccess::ReadWrite, 0);
 
     obj_dict.add(entry);
 
@@ -117,15 +121,17 @@ SCENARIO("A ObjectDictionary object is constructed", "[comstack::canopen::Object
     }
     THEN("When requesting a not added UINT32 entry a empty entry object should be returned")
     {
-      REQUIRE(obj_dict.isEmpty(obj_dict.get_UINT32(0x0001, 0x01)) == true);
+      REQUIRE(obj_dict.get_UINT32(0x0001, 0x01) == 0);
     }
+
+    delete entry; entry = 0;
   }
 
   /************************************************************************************/
 
   WHEN("A STRING obj dict entry is added")
   {
-    ObjectDictionaryEntry<util::type::StaticString> entry(0x1001, 0x01, "CAFFEE", ObjectDictionaryAccess::ReadWrite, 0);
+    ObjectDictionaryEntry<util::type::StaticString> * entry = new ObjectDictionaryEntry<util::type::StaticString>(0x1001, 0x01, "CAFFEE", ObjectDictionaryAccess::ReadWrite, 0);
 
     obj_dict.add(entry);
 
@@ -135,8 +141,10 @@ SCENARIO("A ObjectDictionary object is constructed", "[comstack::canopen::Object
     }
     THEN("When requesting a not added STRING entry a empty entry object should be returned")
     {
-      REQUIRE(obj_dict.isEmpty(obj_dict.get_STRING(0x0001, 0x01)) == true);
+      REQUIRE(obj_dict.get_STRING(0x0001, 0x01) == 0);
     }
+
+    delete entry; entry = 0;
   }
 
   /************************************************************************************/
