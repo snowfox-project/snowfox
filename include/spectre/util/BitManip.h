d060260cbb93d9b2ff1b68363f916c5fc56e4b2f
lxrobotics <cto@lxrobotics.com>
2019-01-02 11:08:46 +0100

    Adding header file BitManip containing constexpr functions for bit manipulations in 8 bit registers

---
+++ include/spectre/util/BitManip.h
@@ -0,0 +1,65 @@
+/**
+ * Spectre is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2018 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SPECTRE_UTIL_BITMANIP_H_
+#define INCLUDE_SPECTRE_UTIL_BITMANIP_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <stdint.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace spectre
+{
+
+namespace util
+{
+
+/**************************************************************************************
+ * FUNCTIONS
+ **************************************************************************************/
+
+constexpr void setBit(volatile uint8_t * reg, uint8_t const bit_pos)
+{
+  *reg |= (1<<bit_pos);
+}
+
+constexpr void clrBit(volatile uint8_t * reg, uint8_t const bit_pos)
+{
+  *reg &= ~(1<<bit_pos);
+}
+
+constexpr void toogleBit(volatile uint8_t * reg, uint8_t const bit_pos)
+{
+  *reg ^= (1<<bit_pos);
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* type*/
+
+} /* spectre */
+
+#endif /* INCLUDE_SPECTRE_UTIL_BITMANIP_H_ */
