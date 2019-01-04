102c1cf85168d137f92dfc1e709135bdb4b04594
lxrobotics <cto@lxrobotics.com>
2019-01-04 09:54:13 +0100

    ATMEGA164P_324P_644P_1284P::ExternalInterruptController - extracting IO bit position definitions

--- include/spectre/cpu/avr/io/common/ATMEGA164P_324P_644P_1284P.h
+++ include/spectre/cpu/avr/io/common/ATMEGA164P_324P_644P_1284P.h
@@ -24,61 +24,101 @@
  **************************************************************************************/
 
 /* EIMSK */
-#define INT2_bp   (2)
-#define INT1_bp   (1)
-#define INT0_bp   (0)
+#define INT2_bp    (2)
+#define INT1_bp    (1)
+#define INT0_bp    (0)
 
 /* PCICR */
-#define PCIE3_bp  (3)
-#define PCIE2_bp  (2)
-#define PCIE1_bp  (1)
-#define PCIE0_bp  (0)
+#define PCIE3_bp   (3)
+#define PCIE2_bp   (2)
+#define PCIE1_bp   (1)
+#define PCIE0_bp   (0)
+
+/* PCMSK0 */
+#define PCINT7_bp  (7)
+#define PCINT6_bp  (6)
+#define PCINT5_bp  (5)
+#define PCINT4_bp  (4)
+#define PCINT3_bp  (3)
+#define PCINT2_bp  (2)
+#define PCINT1_bp  (1)
+#define PCINT0_bp  (0)
+
+/* PCMSK1 */
+#define PCINT15_bp (7)
+#define PCINT14_bp (6)
+#define PCINT13_bp (5)
+#define PCINT12_bp (4)
+#define PCINT11_bp (3)
+#define PCINT10_bp (2)
+#define PCINT9_bp  (1)
+#define PCINT8_bp  (0)
+
+/* PCMSK2 */
+#define PCINT23_bp (7)
+#define PCINT22_bp (6)
+#define PCINT21_bp (5)
+#define PCINT20_bp (4)
+#define PCINT19_bp (3)
+#define PCINT18_bp (2)
+#define PCINT17_bp (1)
+#define PCINT16_bp (0)
+
+/* PCMSK3 */
+#define PCINT31_bp (7)
+#define PCINT30_bp (6)
+#define PCINT29_bp (5)
+#define PCINT28_bp (4)
+#define PCINT27_bp (3)
+#define PCINT26_bp (2)
+#define PCINT25_bp (1)
+#define PCINT24_bp (0)
 
 /* WDTCSR */
-#define WDIE_bp   (6)
+#define WDIE_bp    (6)
 
 /* TIMSK2 */
-#define OCIE2B_bp (2)
-#define OCIE2A_bp (1)
-#define TOIE2_bp  (0)
+#define OCIE2B_bp  (2)
+#define OCIE2A_bp  (1)
+#define TOIE2_bp   (0)
 
 /* TIMSK1 */
-#define ICIE1_bp  (5)
-#define OCIE1B_bp (2)
-#define OCIE1A_bp (1)
-#define TOIE1_bp  (0)
+#define ICIE1_bp   (5)
+#define OCIE1B_bp  (2)
+#define OCIE1A_bp  (1)
+#define TOIE1_bp   (0)
 
 /* TIMSK0 */
-#define OCIE0B_bp (2)
-#define OCIE0A_bp (1)
-#define TOIE0_bp  (0)
+#define OCIE0B_bp  (2)
+#define OCIE0A_bp  (1)
+#define TOIE0_bp   (0)
 
 /* SPCR */
-#define SPIE_bp   (7)
+#define SPIE_bp    (7)
 
 /* UCSR0B */
-#define RXCIE0_bp (7)
-#define TXCIE0_bp (6)
-#define UDRIE0_bp (5)
+#define RXCIE0_bp  (7)
+#define TXCIE0_bp  (6)
+#define UDRIE0_bp  (5)
 
 /* ACSR */
-#define ACIE_bp   (3)
+#define ACIE_bp    (3)
 
 /* ADCSRA */
-#define ADIE_bp   (3)
+#define ADIE_bp    (3)
 
 /* EECR */
-#define EERIE_bp  (3)
+#define EERIE_bp   (3)
 
 /* TWCR */
-#define TWIE_bp   (0)
+#define TWIE_bp    (0)
 
 /* SPMCSR */
-#define SPMIE_bp  (7)
+#define SPMIE_bp   (7)
 
 /* UCSR1B */
-#define RXCIE1_bp (7)
-#define TXCIE1_bp (6)
-#define UDRIE1_bp (5)
+#define RXCIE1_bp  (7)
+#define TXCIE1_bp  (6)
+#define UDRIE1_bp  (5)
 
 #endif /* INCLUDE_SPECTRE_CPU_AVR_IO_COMMON_ATMEGA164P_324P_644P_1284P_H_ */
