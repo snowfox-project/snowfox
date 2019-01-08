abb9f5058d57539f950b2ce84d8459bd7b3b6914
lxrobotics <cto@lxrobotics.com>
2019-01-08 13:31:49 +0100

    ATMEGA16U4_32U4::TIMER0/1/3 - extracting bit pos/mask into common io file

--- include/spectre/cpu/avr/io/common/ATMEGA16U4_32U4.h
+++ include/spectre/cpu/avr/io/common/ATMEGA16U4_32U4.h
@@ -82,6 +82,30 @@
 #define OCIE3A_bp (1)
 #define TOIE3_bp  (0)
 
+/* TCCR0B */
+#define CS02_bp   (2)
+#define CS02_bm   (1<<CS02_bp)
+#define CS01_bp   (1)
+#define CS01_bm   (1<<CS01_bp)
+#define CS00_bp   (0)
+#define CS00_bm   (1<<CS00_bp)
+
+/* TCCR1B */
+#define CS12_bp   (2)
+#define CS12_bm   (1<<CS12_bp)
+#define CS11_bp   (1)
+#define CS11_bm   (1<<CS11_bp)
+#define CS10_bp   (0)
+#define CS10_bm   (1<<CS10_bp)
+
+/* TCCR3B */
+#define CS32_bp   (2)
+#define CS32_bm   (1<<CS32_bp)
+#define CS31_bp   (1)
+#define CS31_bm   (1<<CS31_bp)
+#define CS30_bp   (0)
+#define CS30_bm   (1<<CS30_bp)
+
 /* TWCR */
 #define TWIE_bp   (0)
 
