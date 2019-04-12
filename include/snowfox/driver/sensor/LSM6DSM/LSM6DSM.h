57d93a5fa40ec1cdccbec166b7591bcdaeff07d1
Alexander Entinger <cto@lxrobotics.com>
2019-04-12 11:13:00 +0200

    Adding skeleton for LSM6DSM driver

---
+++ include/snowfox/driver/sensor/LSM6DSM/LSM6DSM.h
@@ -0,0 +1,81 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SNOWFOX_DRIVER_SENSOR_LSM6DSM_LSM6DSM_H_
+#define INCLUDE_SNOWFOX_DRIVER_SENSOR_LSM6DSM_LSM6DSM_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <snowfox/driver/interface/Driver.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace driver
+{
+
+namespace sensor
+{
+
+namespace LSM6DSM
+{
+
+/**************************************************************************************
+ * CONSTANTS
+ **************************************************************************************/
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class LSM6DSM : public driver::interface::Driver
+{
+
+public:
+
+           LSM6DSM();
+  virtual ~LSM6DSM();
+
+
+  virtual bool    open (                                                  ) override;
+  virtual ssize_t read (uint8_t        * buffer, ssize_t const   num_bytes) override;
+  virtual ssize_t write(uint8_t  const * buffer, ssize_t const   num_bytes) override;
+  virtual bool    ioctl(uint32_t const   cmd,    void          * arg      ) override;
+  virtual void    close(                                                  ) override;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* LSM6DSM */
+
+} /* sensor */
+
+} /* driver */
+
+} /* snowfox */
+
+#endif /* INCLUDE_SNOWFOX_DRIVER_SENSOR_LSM6DSM_LSM6DSM_H_ */
---
+++ src/driver/sensor/LSM6DSM/LSM6DSM.cpp
@@ -0,0 +1,94 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <snowfox/driver/sensor/LSM6DSM/LSM6DSM.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace driver
+{
+
+namespace sensor
+{
+
+namespace LSM6DSM
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+LSM6DSM::LSM6DSM()
+{
+
+}
+
+LSM6DSM::~LSM6DSM()
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+bool LSM6DSM::open()
+{
+  /* TODO */ return false;
+}
+
+ssize_t LSM6DSM::read(uint8_t * buffer, ssize_t const num_bytes)
+{
+  /* TODO */ return -1;
+}
+
+ssize_t LSM6DSM::write(uint8_t const * buffer, ssize_t const num_bytes)
+{
+  /* TODO */ return -1;
+}
+
+bool LSM6DSM::ioctl(uint32_t const cmd, void * arg)
+{
+  /* TODO */ return false;
+}
+
+void LSM6DSM::close()
+{
+  /* TODO */
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* LSM6DSM */
+
+} /* sensor */
+
+} /* driver */
+
+} /* snowfox */
