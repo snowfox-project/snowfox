82a36096669e977349004d23ab75b11ce9486bbf
Alexander Entinger <cto@lxrobotics.com>
2019-04-18 08:34:02 +0200

    Using a build matrix for separating semantically different build steps and executing builds in parallel

--- .ci/script/run-cppcheck.sh
+++ .ci/script/run-cppcheck.sh
@@ -2,18 +2,11 @@
 cd $(dirname $(readlink -f $0))
 
 if   [ "$1" = "-out=xml" ]; then
-
   mkdir -p ../../build/report/cppcheck
   cppcheck --enable=warning --xml-version=2 ../src ../../examples 2> ../../build/report/cppcheck/cppcheck.xml
 #  cppcheck-htmlreport --file=cppcheck.xml --report-dir=./ --source-dir=../../../src ../../../examples
-
 elif [ "$1" = "-out=gcc" ]; then
-
   cppcheck --enable=warning --template=gcc ../src ../../examples
-
 else
-
   echo "$(basename "$0") -out=[gcc|xml]" >&2
-
 fi
-
