d13e79601767a0a1264a0d82afa37519c4ce05ca
Alexander Entinger <cto@lxrobotics.com>
2019-05-16 08:53:07 +0200

    Add skeleton for FE310::SpiMaster

---
+++ include/snowfox/hal/riscv64/FE310/SpiMaster.h
@@ -0,0 +1,77 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SNOWFOX_HAL_SIFIVE_FE310_SPIMASTER_H_
+#define INCLUDE_SNOWFOX_HAL_SIFIVE_FE310_SPIMASTER_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <snowfox/hal/interface/spi/SpiMaster.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace hal
+{
+
+namespace FE310
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class SpiMaster : public interface::SpiMaster
+{
+
+public:
+
+           SpiMaster();
+  virtual ~SpiMaster();
+
+
+  /* SPI Master Control Interface */
+
+  virtual uint8_t exchange(uint8_t const data) override;
+
+
+  /* SPI Master Configuration Interface */
+
+  virtual void setSpiMode     (interface::SpiMode     const spi_mode     ) override;
+  virtual void setSpiBitOrder (interface::SpiBitOrder const spi_bit_order) override;
+  virtual void setSpiPrescaler(uint32_t               const spi_prescaler) override;
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* FE310 */
+
+} /* hal */
+
+} /* snowfox */
+
+#endif /* INCLUDE_SNOWFOX_HAL_SIFIVE_FE310_SPIMASTER_H_ */
