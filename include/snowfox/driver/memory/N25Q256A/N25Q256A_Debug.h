93cf1ef10af2d52987613ccc8d9c3f6ad0983c38
Alexander Entinger <cto@lxrobotics.com>
2019-09-17 07:18:39 +0200

    Adding N25Q256A_Debug for printing the relevant memory control registers

---
+++ include/snowfox/driver/memory/N25Q256A/N25Q256A_Debug.h
@@ -0,0 +1,78 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SNOWFOX_DRIVER_MEMORY_N25Q256A_N25Q256A_DEBUG_H_
+#define INCLUDE_SNOWFOX_DRIVER_MEMORY_N25Q256A_N25Q256A_DEBUG_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <snowfox/NoInstance.h>
+
+#include <snowfox/driver/memory/N25Q256A/interface/N25Q256A_Io.h>
+
+#include <snowfox/trace/Trace.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace driver
+{
+
+namespace memory
+{
+
+namespace N25Q256A
+{
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class N25Q256A_Debug : public NoInstance
+{
+
+public:
+
+  static void debug_dumpAllRegs(trace::Trace & trace, interface::N25Q256A_Io & io);
+
+private:
+
+  static void debug_dumpStatusReg  (trace::Trace & trace, interface::N25Q256A_Io & io);
+  static void debug_dumpNVConfigReg(trace::Trace & trace, interface::N25Q256A_Io & io);
+
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* N25Q256A */
+
+} /* memory */
+
+} /* driver */
+
+} /* snowfox */
+
+#endif /* INCLUDE_SNOWFOX_DRIVER_MEMORY_N25Q256A_N25Q256A_DEBUG_H_ */
---
+++ src/driver/memory/N25Q256A/N25Q256A_Debug.cpp
@@ -0,0 +1,77 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <snowfox/driver/memory/N25Q256A/N25Q256A_Debug.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace driver
+{
+
+namespace memory
+{
+
+namespace N25Q256A
+{
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void N25Q256A_Debug::debug_dumpAllRegs(trace::Trace & trace, interface::N25Q256A_Io & io)
+{
+  debug_dumpStatusReg  (trace, io);
+  debug_dumpNVConfigReg(trace, io);
+}
+
+/**************************************************************************************
+ * PRIVATE MEMBER FUNCTIONS
+ **************************************************************************************/
+
+void N25Q256A_Debug::debug_dumpStatusReg(trace::Trace & trace, interface::N25Q256A_Io & io)
+{
+  uint8_t status_reg = io.readStatusReg();
+  trace.println(trace::Level::Debug, "STATUS REG    = %02X", status_reg);
+}
+
+void N25Q256A_Debug::debug_dumpNVConfigReg(trace::Trace & trace, interface::N25Q256A_Io & io)
+{
+  uint16_t non_volatile_config_reg = io.readNVConfigReg();
+  trace.println(trace::Level::Debug, "NV CONFIG REG = %02X", non_volatile_config_reg);
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* N25Q256A */
+
+} /* memory */
+
+} /* driver */
+
+} /* snowfox */
