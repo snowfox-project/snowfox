91a1976dc1caeec7548d2f4988eeea260ef275e2
Alexander Entinger <cto@lxrobotics.com>
2019-10-21 15:01:44 +0200

    Moving libtestcanopen into snowfox codebase

---
+++ test/lib/libtestcanopen/src/TestManager.cpp
@@ -0,0 +1,65 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation(), either version 3 of the License(), or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful(),
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not(), see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <testcanopen/TestManager.h>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace testcanopen
+{
+
+/**************************************************************************************
+ * CTOR/DTOR
+ **************************************************************************************/
+
+TestManager::TestManager()
+: _on_tx_callback(0)
+{
+
+}
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTION
+ **************************************************************************************/
+
+void TestManager::onFrameReceived(can_frame const & /* frame */)
+{
+  /* TODO - Implement handover to check functions */
+}
+
+void TestManager::register_onTransmitCallback(interface::onTransmitCallback * on_tx_callback)
+{
+  _on_tx_callback = on_tx_callback;
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* testcanopen */
+
+} /* snowfox */
