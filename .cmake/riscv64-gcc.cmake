8910c1fc694f8bd76d442ff173648ed1f2a4705a
Alexander Entinger <cto@lxrobotics.com>
2019-10-28 16:14:47 +0100

    Filling out CMakeLists for SiFive FE310

--- .cmake/riscv64-gcc.cmake
+++ .cmake/riscv64-gcc.cmake
@@ -5,6 +5,19 @@ set(CMAKE_CXX_COMPILER riscv64-unknown-elf-g++)
 
 ##########################################################################
 
+set(SNOWFOX_RISCV64_CMAKE_ASM_FLAGS       "-march=rv32imac -mabi=ilp32 -mcmodel=medlow")
+set(SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS     "")
+set(SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS   "")
+set(SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -Og -ggdb -ffunction-sections -fdata-sections")
+
+##########################################################################
+
+set(SNOWFOX_CMAKE_ASM_FLAGS ${SNOWFOX_RISCV64_CMAKE_ASM_FLAGS})
+set(SNOWFOX_CMAKE_C_FLAGS   "${SNOWFOX_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
+set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
+
+##########################################################################
+
 set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-Wl,--relax,--gc-sections")
 
 ##########################################################################
