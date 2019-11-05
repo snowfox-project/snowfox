89bddf65968be2de382006686967d7039fc45730
Alexander Entinger <cto@lxrobotics.com>
2019-11-05 08:03:21 +0100

    Providing ability for all MCU toolchains (AVR/RISCV64) to set assembly flags

--- .cmake/avr-gcc.cmake
+++ .cmake/avr-gcc.cmake
@@ -5,12 +5,14 @@ set(CMAKE_CXX_COMPILER avr-g++)
 
 ##########################################################################
 
+set(SNOWFOX_AVR_GCC_CMAKE_ASM_FLAGS   "")
 set(SNOWFOX_AVR_GCC_CMAKE_C_FLAGS     "")
 set(SNOWFOX_AVR_GCC_CMAKE_CXX_FLAGS   "")
 set(SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS "-mmcu=${MCU_TYPE} -Og -ggdb -funsigned-char -funsigned-bitfields -fshort-enums -ffunction-sections -fdata-sections -fno-exceptions")
 
 ##########################################################################
 
+set(SNOWFOX_CMAKE_ASM_FLAGS "${SNOWFOX_CMAKE_ASM_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_ASM_FLAGS}")
 set(SNOWFOX_CMAKE_C_FLAGS   "${SNOWFOX_CMAKE_C_FLAGS}   ${SNOWFOX_AVR_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS}")
 set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS}")
  
--- .cmake/riscv64-gcc.cmake
+++ .cmake/riscv64-gcc.cmake
@@ -5,14 +5,14 @@ set(CMAKE_CXX_COMPILER riscv64-unknown-elf-g++)
 
 ##########################################################################
 
-set(SNOWFOX_RISCV64_CMAKE_ASM_FLAGS       "-march=rv32imac -mabi=ilp32 -mcmodel=medlow")
+set(SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS   "-march=rv32imac -mabi=ilp32 -mcmodel=medlow")
 set(SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS     "")
 set(SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS   "")
 set(SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -O1 -g3 -ffunction-sections -fdata-sections -fno-exceptions")
 
 ##########################################################################
 
-set(SNOWFOX_CMAKE_ASM_FLAGS ${SNOWFOX_RISCV64_CMAKE_ASM_FLAGS})
+set(SNOWFOX_CMAKE_ASM_FLAGS "${SNOWFOX_CMAKE_ASM_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS}")
 set(SNOWFOX_CMAKE_C_FLAGS   "${SNOWFOX_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
 set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
 
