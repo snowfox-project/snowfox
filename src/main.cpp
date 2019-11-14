1bb76dd511c46070aae36e0730fcacab0c4910f7
Alexander Entinger <cto@lxrobotics.com>
2019-11-14 14:34:04 +0100

    Restructuring snowfox implementation - it's no longer just a library but also builds directly the firmware binary - this has the added benefit of not needing to duplicate the build flags in the cmake files of the final binaries

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**************************************************************************************
+ * FUNCTION DECLARATION
+ **************************************************************************************/
+
+int snowfox_main() __attribute__((weak));
+
+/**************************************************************************************
+ * MAIN
+ **************************************************************************************/
+
+int main()
+{
+  return snowfox_main();
+}
+
+/**************************************************************************************
+ * FUNCTION IMPLEMENTATION
+ **************************************************************************************/
+
+/* Should no one bother to override it, then this is the actual
+ * Snowfox enabled application running bare metal on your MCU.
+ */
+int snowfox_main()
+{
+  for(;;) { }
+}
