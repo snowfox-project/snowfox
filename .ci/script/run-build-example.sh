b2dbe63de7ac5fb5737023676e7abbb82f985964
Alexander Entinger <cto@lxrobotics.com>
2019-11-15 10:10:05 +0100

    Adjusting build script for examples to new situation

--- .ci/script/run-build-example.sh
+++ .ci/script/run-build-example.sh
@@ -9,8 +9,10 @@ cd ../..
 
 cp "$1"/config.cmake config.cmake
 
-(rm -rf build && mkdir build && cd build; cmake ..; make -j4; sudo make install)
-(cd "$1"; rm -rf build && mkdir -p build && cd build; cmake ..; make -j4)
-(cd build; sudo make uninstall)
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make -j4
 
 #######################################################################################################
