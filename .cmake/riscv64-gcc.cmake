34335ea7a16f3e0fb4faa08f3b48db3bb7b1fdc1
Alexander Entinger <cto@lxrobotics.com>
2019-12-09 14:22:29 +0100

    Moving ldscript for FE310 up from examples and into folder ldscript/riscv64/FE310 and adjusting build scripts accordingly

--- .cmake/riscv64-gcc.cmake
+++ .cmake/riscv64-gcc.cmake
@@ -9,12 +9,14 @@ set(SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS   "-march=rv32imac -mabi=ilp32 -mcmodel=
 set(SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS     "")
 set(SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS   "")
 set(SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -fno-exceptions")
+set(SNOWFOX_RISCV64_GCC_CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections,-Map=${SNOWFOX_APPLICATON_TARGET}.map -nostartfiles -T ../ldscript/riscv64/FE310/FE310-G002.ld")
 
 ##########################################################################
 
 set(SNOWFOX_CMAKE_ASM_FLAGS "${SNOWFOX_CMAKE_ASM_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS}")
 set(SNOWFOX_CMAKE_C_FLAGS   "${SNOWFOX_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
 set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
+set(SNOWFOX_CMAKE_EXE_LINKER_FLAGS ${SNOWFOX_RISCV64_GCC_CMAKE_EXE_LINKER_FLAGS})
 
 ##########################################################################
 
