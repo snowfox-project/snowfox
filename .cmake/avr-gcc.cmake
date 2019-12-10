b9dcd527ffb92b80a53fadfdfd971e57b2fca204
Alexander Entinger <cto@lxrobotics.com>
2019-12-10 15:34:20 +0100

    Simplifying cmake files

--- .cmake/avr-gcc.cmake
+++ .cmake/avr-gcc.cmake
@@ -5,16 +5,7 @@ set(CMAKE_CXX_COMPILER avr-g++)
 
 ##########################################################################
 
-set(SNOWFOX_AVR_GCC_CMAKE_ASM_FLAGS   "")
-set(SNOWFOX_AVR_GCC_CMAKE_C_FLAGS     "")
-set(SNOWFOX_AVR_GCC_CMAKE_CXX_FLAGS   "")
-set(SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS "-mmcu=${MCU_TYPE} -fno-exceptions")
-
-##########################################################################
-
-set(SNOWFOX_CMAKE_ASM_FLAGS "${SNOWFOX_CMAKE_ASM_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_ASM_FLAGS}")
-set(SNOWFOX_CMAKE_C_FLAGS   "${SNOWFOX_CMAKE_C_FLAGS}   ${SNOWFOX_AVR_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS}")
-set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_AVR_GCC_CMAKE_C_CXX_FLAGS}")
+set(SNOWFOX_CMAKE_CXX_FLAGS "${SNOWFOX_CMAKE_CXX_FLAGS} -mmcu=${MCU_TYPE} -fno-exceptions")
  
 ##########################################################################
 
--- .cmake/riscv64-gcc.cmake
+++ .cmake/riscv64-gcc.cmake
@@ -5,18 +5,8 @@ set(CMAKE_CXX_COMPILER riscv64-unknown-elf-g++)
 
 ##########################################################################
 
-set(SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS        "-march=rv32imac -mabi=ilp32 -mcmodel=medlow")
-set(SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS          "")
-set(SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS        "")
-set(SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS      "-march=rv32imac -mabi=ilp32 -mcmodel=medlow -fno-exceptions")
-set(SNOWFOX_RISCV64_GCC_CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections,-Map=${SNOWFOX_APPLICATON_TARGET}.map -nostartfiles -T ../ldscript/riscv64/FE310/FE310-G002.ld")
-
-##########################################################################
-
-set(SNOWFOX_CMAKE_ASM_FLAGS        "${SNOWFOX_CMAKE_ASM_FLAGS}        ${SNOWFOX_RISCV64_GCC_CMAKE_ASM_FLAGS}")
-set(SNOWFOX_CMAKE_C_FLAGS          "${SNOWFOX_CMAKE_C_FLAGS}          ${SNOWFOX_RISCV64_GCC_CMAKE_C_FLAGS}   ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
-set(SNOWFOX_CMAKE_CXX_FLAGS        "${SNOWFOX_CMAKE_CXX_FLAGS}        ${SNOWFOX_RISCV64_GCC_CMAKE_CXX_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_C_CXX_FLAGS}")
-set(SNOWFOX_CMAKE_EXE_LINKER_FLAGS "${SNOWFOX_CMAKE_EXE_LINKER_FLAGS} ${SNOWFOX_RISCV64_GCC_CMAKE_EXE_LINKER_FLAGS}")
+set(SNOWFOX_CMAKE_CXX_FLAGS        "${SNOWFOX_CMAKE_CXX_FLAGS} -march=rv32imac -mabi=ilp32 -mcmodel=medlow -fno-exceptions")
+set(SNOWFOX_CMAKE_EXE_LINKER_FLAGS "${SNOWFOX_CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections,-Map=${SNOWFOX_APPLICATON_TARGET}.map -nostartfiles -T ../ldscript/riscv64/FE310/FE310-G002.ld")
 
 ##########################################################################
 
