0dfaa90459c167718cf35fbf4d00b936cef6e59f
Alexander Entinger <cto@lxrobotics.com>
2020-03-24 06:07:50 +0100

    Adding methods for converting enum classes into integer values, bitpos and bitmaps without using static_cast

---
+++ include/snowfox/util/EnumClassConv.hpp
@@ -0,0 +1,62 @@
+/**
+ * Snowfox is a modular RTOS with extensive IO support.
+ * Copyright (C) 2017 - 2020 Alexander Entinger / LXRobotics GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INCLUDE_SNOWFOX_UTIL_ENUM_CLASS_CONV_H_
+#define INCLUDE_SNOWFOX_UTIL_ENUM_CLASS_CONV_H_
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace snowfox
+{
+
+namespace util
+{
+
+/**************************************************************************************
+ * CONVERSION FUNCTIONS
+ **************************************************************************************/
+
+template <typename Enumeration>
+constexpr auto to_integer(Enumeration const value) -> typename std::underlying_type<Enumeration>::type
+{
+  return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+}
+
+template <typename Enumeration>
+constexpr auto bp(Enumeration const value) -> typename std::underlying_type<Enumeration>::type
+{
+  return to_integer(value);
+}
+
+template <typename Enumeration>
+constexpr auto bm(Enumeration const value) -> typename std::underlying_type<Enumeration>::type
+{
+    return (1 << to_integer(value));
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* util*/
+
+} /* snowfox */
+
+#endif /* INCLUDE_SNOWFOX_UTIL_ENUM_CLASS_CONV_H_ */
