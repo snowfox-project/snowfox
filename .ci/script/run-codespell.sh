c2c0b67982e4605ff86208c188b9541dcdcb4b9b
Alexander Entinger <cto@lxrobotics.com>
2020-04-14 13:57:09 +0200

    Ignore external directory for running codespell

--- .ci/script/run-codespell.sh
+++ .ci/script/run-codespell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-codespell --skip="$1/.git" --skip="$1/test/external" --ignore-words="$1/.ci/codespell/ignore-words-list.txt" "$1"
+codespell --skip="$1/.git" --skip="$1/external" --skip="$1/test/external" --ignore-words="$1/.ci/codespell/ignore-words-list.txt" "$1"
